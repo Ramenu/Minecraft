6760d41d877983375af4265e602ca57127933e78
Ramenu <abdurpro@gmail.com>
2022-02-20 18:37:49 -0500

    Removed unnecessary inline

--- include/mylib/gfx/texture.hpp
+++ include/mylib/gfx/texture.hpp
@@ -7,8 +7,8 @@
 extern void loadTexture(const char* fileName);
 extern void loadImage(const char* fileName, ImageData& image);
 extern void createTexture(const char* filePath, uint32_t& texture);
-inline constexpr uint16_t atlasWidth {2}, atlasHeight {2};
-inline constexpr float xPos {1.0f / atlasWidth}, yPos {1.0f / atlasHeight};
+constexpr uint16_t atlasWidth {2}, atlasHeight {2};
+constexpr float xPos {1.0f / atlasWidth}, yPos {1.0f / atlasHeight};
 
 
 #endif // TEXTURE_HPP
