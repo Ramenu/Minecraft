76c6ba96e28b49af4a060eee38b8fcfff7d52ef5
Ramenu <abdurpro@gmail.com>
2022-02-21 16:22:43 -0500

    added assert to make sure size_t is big enough

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 
 int main()
 {
+    static_assert(sizeof(size_t) >= 4, "Integer must be at least 4 bytes to run program.");
     constexpr uint16_t windowWidth {400}, windowHeight {400};
     auto minecraft = std::make_unique<Game>(windowWidth, windowHeight);
     minecraft->runGame();
