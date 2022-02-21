6104105fb796f6d312f5a08dd542e6069d171bd8
Ramenu <abdurpro@gmail.com>
2022-02-20 20:40:10 -0500

    some members automatically initialized

--- include/mylib/camera.hpp
+++ include/mylib/camera.hpp
@@ -2,30 +2,31 @@
 #define CAMERA_HPP
 
 #include "mylib/physics/ray.hpp"
+#include "glm/gtc/matrix_transform.hpp"
 
 class Camera
 {
     public:
-    	Camera(float cameraYaw, float cameraPitch, float cameraMovementSpeed, float cameraMouseSensitivty, float mouseZoom);
+    	Camera(float yaw, float pitch, float speed, float sensitivity, float zoom);
 		~Camera();
-		glm::vec3 cameraPos;
-		glm::vec3 cameraFront;
-		glm::vec3 cameraRight;
-		glm::mat4 view;
+		glm::vec3 cameraPos {glm::vec3(0.0f, 0.0f, 3.0f)};
+		glm::vec3 cameraFront {glm::vec3(0.0f, 0.0f, -1.0f)};
+		glm::vec3 cameraRight {glm::normalize(glm::cross(cameraFront, cameraUp)) * movementSpeed};
+		glm::mat4 view {glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)};
 		void updateCameraPos();
-		float movementSpeed;
-		float deltaTime;
-		float lastFrame;
 		float yaw;
 		float pitch;
+		float movementSpeed;
 		float mouseSensitivity;
 		float zoom;
-		bool firstMouseMovement;
+		float deltaTime {0.0f};
+		float lastFrame {0.0f};
+	private:
+		bool firstMouseMovement {true};
 		double lastX;
 		double lastY;
-	private:
-		glm::vec3 cameraUp;
-		Ray cameraRay;
+		glm::vec3 cameraUp {glm::vec3(0.0f, 1.0f, 0.0f)};
+		Ray cameraRay {cameraPos, cameraFront, glm::vec3(0.0f, 0.0f, 2.0f)};
 };
 
 #endif // CAMERA_HPP
