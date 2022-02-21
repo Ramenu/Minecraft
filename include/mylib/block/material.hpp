79d1f859b9b71af2e97963c6f380de157087f63c
Ramenu <abdurpro@gmail.com>
2022-02-20 20:39:12 -0500

    members automatically initialized

--- include/mylib/block/material.hpp
+++ include/mylib/block/material.hpp
@@ -7,11 +7,11 @@ class Material
 {
     public:
         Material();
-        Material(const glm::vec3& ambientVec, const glm::vec3& specularVec, uint16_t diffuseNum, float shineNum);
-        glm::vec3 ambient;
-        glm::vec3 specular;
-        unsigned int diffuse;
-        float shine;
+        Material(const glm::vec3& ambient, const glm::vec3& specular, uint16_t diffuse, float shine);
+        glm::vec3 ambient {};
+        glm::vec3 specular {};
+        uint16_t diffuse {};
+        float shine {};
 };
 
 #endif // MATERIAL_HPP
