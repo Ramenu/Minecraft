94e605296dd5da411f851ac82c18fb6fab20d8be
Ramenu <abdurpro@gmail.com>
2022-02-22 21:35:44 -0500

    added bufferdata

--- include/mylib/buffers/buffer.hpp
+++ include/mylib/buffers/buffer.hpp
@@ -3,10 +3,19 @@
 
 #include <cstdint>
 
+struct BufferData
+{
+    uint32_t buffer;
+    uint32_t size;
+    const void* data;
+    bool isStatic;
+    bool isVertexBuffer;
+};
+
 namespace Buffer
 {
-    extern void loadVertexBuffer(uint32_t& buffer, uint32_t size, const void* data);
-    extern void loadElementBuffer(uint32_t& buffer, uint32_t size, const void* data);
+    extern void createBuffer(BufferData& buffer);
+    extern void updateBuffer(const BufferData& buffer);
 }
 
 #endif // BUFFER_HPP
--- src/buffer.cpp
+++ src/buffer.cpp
@@ -3,17 +3,21 @@
 
 namespace Buffer
 {
-    /* Loads the data into the vertex buffer passed. */
-    void loadVertexBuffer(uint32_t& buffer, uint32_t size, const void* data)
+    /* Loads the data into the buffer passed. Should be called during the initialization of a buffer object only. */
+    void createBuffer(BufferData& buffer)
     {
-        glBindBuffer(GL_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+        int bufferType = (buffer.isVertexBuffer) ? GL_ARRAY_BUFFER : GL_ELEMENT_ARRAY_BUFFER;
+        int draw = (buffer.isStatic) ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW;
+        glGenBuffers(1, &buffer.buffer);
+        glBindBuffer(bufferType, buffer.buffer);
+        glBufferData(bufferType, buffer.size, buffer.data, draw);
     }
 
-    /* Loads the data into the element buffer passed. */
-    void loadElementBuffer(uint32_t& buffer, uint32_t size, const void* data)
+    /* Updates the data of the buffer. Should only be used for dynamic buffer. Function assumes that the buffer
+       passed is a vertex buffer. */
+    void updateBuffer(const BufferData& buffer)
     {
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, buffer);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, buffer.buffer);
+        glBufferData(GL_ARRAY_BUFFER, buffer.size, buffer.data, GL_DYNAMIC_DRAW);
     }
 }
