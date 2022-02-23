7cc9bb23a541d83b77a988475b333161c7fd552b
Ramenu <abdurpro@gmail.com>
2022-02-22 21:39:39 -0500

    changed window width/height

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     static_assert(sizeof(size_t) >= 4, "Integer must be at least 4 bytes to run program.");
-    constexpr uint16_t windowWidth {400}, windowHeight {400};
+    constexpr uint16_t windowWidth {512}, windowHeight {512};
     Game minecraft {windowWidth, windowHeight};
     minecraft.runGame();
     return 0;
