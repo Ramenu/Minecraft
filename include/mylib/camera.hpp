daa5624eb7bd3101f2debe89882c0d5aeb0bf152
Ramenu <abdurpro@gmail.com>
2022-02-22 21:35:14 -0500

    removed redundant constructors

--- include/mylib/camera.hpp
+++ include/mylib/camera.hpp
@@ -9,8 +9,8 @@ class Camera
     public:
     	Camera(float yaw, float pitch, float speed, float sensitivity, float zoom);
 		~Camera() = default;
-		glm::vec3 cameraPos {glm::vec3{0.0f, 0.0f, 3.0f}};
-		glm::vec3 cameraFront {glm::vec3{0.0f, 0.0f, -1.0f}};
+		glm::vec3 cameraPos {0.0f, 0.0f, 3.0f};
+		glm::vec3 cameraFront {0.0f, 0.0f, -1.0f};
 		glm::vec3 cameraRight {glm::normalize(glm::cross(cameraFront, cameraUp)) * movementSpeed};
 		glm::mat4 view {glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp)};
 		void updateCameraPos();
@@ -25,8 +25,8 @@ class Camera
 		bool firstMouseMovement {true};
 		double lastX;
 		double lastY;
-		glm::vec3 cameraUp {glm::vec3(0.0f, 1.0f, 0.0f)};
-		Ray cameraRay {cameraPos, cameraFront, glm::vec3(0.0f, 0.0f, 2.0f)};
+		glm::vec3 cameraUp {0.0f, 1.0f, 0.0f};
+		Ray cameraRay {cameraPos, cameraFront, glm::vec3{0.0f, 0.0f, 2.0f}};
 };
 
 #endif // CAMERA_HPP
