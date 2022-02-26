b3e7437c130fc2c67b649a1a0b1bbdbb43b854c2
Ramenu <abdurpro@gmail.com>
2022-02-26 15:27:29 -0500

    updated vertices

--- src/vertices.cpp
+++ src/vertices.cpp
@@ -1,49 +1,53 @@
-#include "mylib/vertices.hpp"
-#include "mylib/gfx/texture.hpp"
+#include "minecraft/vertices.hpp"
+#include "minecraft/gfx/texture.hpp"
 
 using namespace Texture;
 
+constexpr float xStrideToNextBlock {atlasWidth - 1};
+
+// For future reference to the y coordinates, just adjust them inside the vertex shader
+// using the .y attribute
 constexpr float cubeVertices[288] =
 {
-   -0.5f, -0.5f, 0.0f,    0.0f, yPos,   0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.0f,    0.0f, 0.0f,   0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.0f,    xPos, yPos,   0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.0f,    xPos, yPos,   0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.0f,    0.0f, 0.0f,   0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.0f,    xPos, 0.0f,   0.0f, 0.0f,  1.0f,
+   -0.5f, -0.5f, 0.0f,    0.0f,                      yPos,         0.0f, 0.0f,  1.0f,
+   -0.5f,  0.5f, 0.0f,    0.0f,                      0.0f,         0.0f, 0.0f,  1.0f,
+    0.0f, -0.5f, 0.0f,    xPos,                      yPos,         0.0f, 0.0f,  1.0f,
+    0.0f, -0.5f, 0.0f,    xPos,                      yPos,         0.0f, 0.0f,  1.0f,
+   -0.5f,  0.5f, 0.0f,    0.0f,                      0.0f,         0.0f, 0.0f,  1.0f,
+    0.0f,  0.5f, 0.0f,    xPos,                      0.0f,         0.0f, 0.0f,  1.0f,
 
-   -0.5f, -0.5f, 0.5f,    0.0f, yPos,   0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.5f,    0.0f, 0.0f,   0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.5f,    xPos, yPos,   0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.5f,    xPos, yPos,   0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.5f,    0.0f, 0.0f,   0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.5f,    xPos, 0.0f,   0.0f, 0.0f,  1.0f,
+   -0.5f, -0.5f, 0.5f,    0.0f,                      yPos,         0.0f, 0.0f,  1.0f,
+   -0.5f,  0.5f, 0.5f,    0.0f,                      0.0f,         0.0f, 0.0f,  1.0f,
+    0.0f, -0.5f, 0.5f,    xPos,                      yPos,         0.0f, 0.0f,  1.0f,
+    0.0f, -0.5f, 0.5f,    xPos,                      yPos,         0.0f, 0.0f,  1.0f,
+   -0.5f,  0.5f, 0.5f,    0.0f,                      0.0f,         0.0f, 0.0f,  1.0f,
+    0.0f,  0.5f, 0.5f,    xPos,                      0.0f,         0.0f, 0.0f,  1.0f,
 
-   -0.5f, -0.5f, 0.5f,	  0.0f, yPos,   1.0f, 0.0f,  0.0f,  
-   -0.5f,  0.5f, 0.5f,	  0.0f, 0.0f,   1.0f, 0.0f,  0.0f, 
-   -0.5f, -0.5f, 0.0f,    xPos, yPos,   1.0f, 0.0f,  0.0f,   
-   -0.5f, -0.5f, 0.0f,	  xPos, yPos,   1.0f, 0.0f,  0.0f,  
-   -0.5f,  0.5f, 0.5f,	  0.0f, 0.0f,   1.0f, 0.0f,  0.0f,  
-   -0.5f,  0.5f, 0.0f,	  xPos, 0.0f,   1.0f, 0.0f,  0.0f,
+   -0.5f, -0.5f, 0.5f,	  0.0f,                      yPos,         1.0f, 0.0f,  0.0f,  
+   -0.5f,  0.5f, 0.5f,	  0.0f,                      0.0f,         1.0f, 0.0f,  0.0f, 
+   -0.5f, -0.5f, 0.0f,    xPos,                      yPos,         1.0f, 0.0f,  0.0f,   
+   -0.5f, -0.5f, 0.0f,	  xPos,                      yPos,         1.0f, 0.0f,  0.0f,  
+   -0.5f,  0.5f, 0.5f,	  0.0f,                      0.0f,         1.0f, 0.0f,  0.0f,  
+   -0.5f,  0.5f, 0.0f,	  xPos,                      0.0f,         1.0f, 0.0f,  0.0f,
 
-    0.0f, -0.5f, 0.5f,    0.0f, yPos,   1.0f, 0.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
-    0.0f, -0.5f, 0.0f,    xPos, yPos,   1.0f, 0.0f, 0.0f,
-    0.0f, -0.5f, 0.0f,    xPos, yPos,   1.0f, 0.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    0.0f, 0.0f,   1.0f, 0.0f, 0.0f,
-    0.0f,  0.5f, 0.0f,    xPos, 0.0f,   1.0f, 0.0f, 0.0f,
+    0.0f, -0.5f, 0.5f,    0.0f,                      yPos,         1.0f, 0.0f, 0.0f,
+    0.0f,  0.5f, 0.5f,    0.0f,                      0.0f,         1.0f, 0.0f, 0.0f,
+    0.0f, -0.5f, 0.0f,    xPos,                      yPos,         1.0f, 0.0f, 0.0f,
+    0.0f, -0.5f, 0.0f,    xPos,                      yPos,         1.0f, 0.0f, 0.0f,
+    0.0f,  0.5f, 0.5f,    0.0f,                      0.0f,         1.0f, 0.0f, 0.0f,
+    0.0f,  0.5f, 0.0f,    xPos,                      0.0f,         1.0f, 0.0f, 0.0f,
 
-   -0.5f,  0.5f, 0.5f,    xPos,     yPos,   0.0f,  1.0f, 0.0f, 
-   -0.5f,  0.5f, 0.0f,    xPos,     0.0f,   0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    xPos * 2, yPos,   0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    xPos * 2, yPos,   0.0f,  1.0f, 0.0f,
-   -0.5f,  0.5f, 0.0f,    xPos,     0.0f,   0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.0f,    xPos * 2, 0.0f,   0.0f,  1.0f, 0.0f,
+   -0.5f,  0.5f, 0.5f,    xPos,                      yPos,         0.0f,  1.0f, 0.0f, 
+   -0.5f,  0.5f, 0.0f,    xPos,                      0.0f,         0.0f,  1.0f, 0.0f,
+    0.0f,  0.5f, 0.5f,    xPos * xStrideToNextBlock, yPos,         0.0f,  1.0f, 0.0f,
+    0.0f,  0.5f, 0.5f,    xPos * xStrideToNextBlock, yPos,         0.0f,  1.0f, 0.0f,
+   -0.5f,  0.5f, 0.0f,    xPos,                      0.0f,         0.0f,  1.0f, 0.0f,
+    0.0f,  0.5f, 0.0f,    xPos * xStrideToNextBlock, 0.0f,         0.0f,  1.0f, 0.0f,
 
-   -0.5f, -0.5f, 0.5f,    0.0f, yPos * 2,   0.0f, -1.0f, 0.0f,   
-   -0.5f, -0.5f, 0.0f,    0.0f, yPos,       0.0f, -1.0f, 0.0f,
-    0.0f, -0.5f, 0.5f,    xPos, yPos * 2,   0.0f, -1.0f, 0.0f,   
-    0.0f, -0.5f, 0.5f,    xPos, yPos * 2,   0.0f, -1.0f, 0.0f,   
-   -0.5f, -0.5f, 0.0f,    0.0f, yPos,       0.0f, -1.0f, 0.0f,
-    0.0f, -0.5f, 0.0f,    xPos, yPos,       0.0f, -1.0f, 0.0f,
+   -0.5f, -0.5f, 0.5f,    xPos * xStrideToNextBlock, yPos,         0.0f, -1.0f, 0.0f,   
+   -0.5f, -0.5f, 0.0f,    xPos * xStrideToNextBlock, 0.0f,         0.0f, -1.0f, 0.0f,
+    0.0f, -0.5f, 0.5f,    xPos * atlasWidth,         yPos,         0.0f, -1.0f, 0.0f,                  
+    0.0f, -0.5f, 0.5f,    xPos * atlasWidth,         yPos,         0.0f, -1.0f, 0.0f,                  
+   -0.5f, -0.5f, 0.0f,    xPos * xStrideToNextBlock, 0.0f,         0.0f, -1.0f, 0.0f,               
+    0.0f, -0.5f, 0.0f,    xPos * xStrideToNextBlock, 0.0f,         0.0f, -1.0f, 0.0f,               
 };
