9558e483c68910213bb04a4b2b01d58f8833a62d
Ramenu <abdurpro@gmail.com>
2022-02-26 15:25:22 -0500

    updated atlasWidth and atlasHeight

---
+++ include/minecraft/gfx/texture.hpp
@@ -0,0 +1,20 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include <cstdint>
+#include "minecraft/data/imagedata.h"
+
+namespace Texture
+{
+    extern void loadTexture(const char* fileName);
+    [[nodiscard]] extern ImageData loadImage(const char* fileName);
+    extern void createTexture(const char* filePath, uint32_t& texture);
+    constexpr uint16_t atlasWidth {3}, atlasHeight {2};
+    constexpr float xPos {1.0f / atlasWidth}, yPos {1.0f / atlasHeight};
+    extern uint32_t getTextureAtlas();
+    extern void initTextureAtlas();
+    extern void deleteTextureAtlas();
+}
+
+
+#endif // TEXTURE_HPP
