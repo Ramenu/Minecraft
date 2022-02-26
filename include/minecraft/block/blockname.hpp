c8cae4fc76fa7d300e6cd6bfda869eccd0e7652a
Ramenu <abdurpro@gmail.com>
2022-02-26 15:21:03 -0500

    updated include directory

---
+++ include/minecraft/block/blockname.hpp
@@ -0,0 +1,10 @@
+#ifndef BLOCKNAME_HPP
+#define BLOCKNAME_HPP
+
+enum class BlockName
+{
+    Grass_Block,
+    Cobblestone_Block
+};
+
+#endif // BLOCKNAME_HPP
---
+++ include/minecraft/vertices.hpp
@@ -0,0 +1,6 @@
+#ifndef VERTICES_HPP
+#define VERTICES_HPP
+
+extern const float cubeVertices[288];
+
+#endif // VERTICES_HPP
