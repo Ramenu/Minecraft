37ec1c75f6bc54f2add28d732079e69bbceeaee7
Ramenu <abdurpro@gmail.com>
2022-03-15 14:14:15 -0400

    made blocksounds static

--- src/sound.cpp
+++ src/sound.cpp
@@ -4,7 +4,7 @@
 // An array that holds a pair of block sounds, the first element in the pair has a file path to the block's
 // initialization sound. The second element contains a file path to the block's deletion sound. Examples of
 // when these are played is when a player places or destroys a block.
-std::pair<SoundData, SoundData> blockSounds[2] {
+static std::pair<SoundData, SoundData> blockSounds[2] {
     std::pair<SoundData, SoundData>{"./sfx/grassblock/grassplace.ogg", "./sfx/grassblock/grassbreak.ogg"}, // Grass block sounds
     std::pair<SoundData, SoundData>{"./sfx/stone/stoneplace.ogg", "./sfx/stone/stonebreak.ogg"} // Stone block sounds
 };
