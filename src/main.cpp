c46d0a3cf1970c16b64b264aec1f47a540b7ffda
Ramenu <abdurpro@gmail.com>
2022-03-15 14:12:39 -0400

    enabled VTP to be enabled if compiled on windows

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,19 @@ extern "C"
 int main()
 {
     static_assert(sizeof(size_t) >= 4, "Go buy a real computer");
-    if (enable_virtual_terminal_processing()) // ASCI colors don't work atm ;(
-    {
+
+    #ifdef _WIN32
+        if (enable_virtual_terminal_processing()) // ASCI colors don't work atm ;(
+        {
+            Game minecraft {"Minecraft"};
+            minecraft.runGame();
+        } 
+        else
+            return 1;
+    #else
         Game minecraft {"Minecraft"};
         minecraft.runGame();
-    }
+    #endif
+    
     return 0;
 }
