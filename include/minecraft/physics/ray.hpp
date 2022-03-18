8b8ae287f84670f546a26425176fa7f9537561f9
Ramenu <abdurpro@gmail.com>
2022-03-18 15:02:54 -0400

    removed useless shaders and buffers

--- include/minecraft/physics/ray.hpp
+++ include/minecraft/physics/ray.hpp
@@ -8,17 +8,13 @@ class Ray
 {
     public:
         Ray(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection, const glm::vec3 &rayLength);
-        ~Ray() noexcept;
         void updateRay();
         inline glm::vec3 getRay() const {return ray;}
         glm::vec3 origin;
         glm::vec3 direction;
-        Shader shader;
         bool intersectsWith(const glm::vec3 &b) const noexcept;
     private:
         glm::vec3 ray {};
-        uint32_t vao {};
-        BufferData vertexBuffer {};
         glm::vec3 length;
 };
 
--- src/ray.cpp
+++ src/ray.cpp
@@ -1,11 +1,7 @@
 #include "minecraft/physics/ray.hpp"
-#include "minecraft/window.hpp"
-#include "minecraft/attribute.hpp"
-#include "minecraft/renderer.hpp"
 #if 0
     #include <iostream>
 #endif
-#include <string>
 
 /**
  * Initializes the origin of the ray, its direction 
@@ -15,38 +11,9 @@
 Ray::Ray(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection, const glm::vec3 &rayLength) : 
 origin{rayOrigin}, 
 direction{rayDirection}, 
-shader{"shaders/line/linevertexshader.vert", "shaders/line/linefragmentshader.frag"}, 
 length{rayLength}
 {
-    // Create vertex array and buffer
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao);
-    const glm::vec3 rayEnd{origin + direction * length};
-    #if 1
-        const float rayVertices[6] =
-        {
-            origin.x, origin.y, origin.z,
-            rayEnd.x, rayEnd.y, 0.0f
-        };
-    #endif
-    uint32_t buffer {};
-    const bool staticDrawEnabled {true};
-    vertexBuffer = BufferData{buffer, sizeof(rayVertices), static_cast<const float*>(rayVertices), staticDrawEnabled};
-    Buffer::createBuffer(vertexBuffer);
-    setAttributes(std::vector<int32_t>{3});
-    enableVAOAttributes({0,1});
-    shader.useShader();
-    shader.setMat4("projection", Renderer::projection);
 
-}
-
-/**
- * Deletes the vertex buffer and vertex array upon destruction.
- */
-Ray::~Ray() noexcept
-{
-    glDeleteBuffers(1, &vertexBuffer.buffer);
-    glDeleteVertexArrays(1, &vao);
 }
 
 /**
