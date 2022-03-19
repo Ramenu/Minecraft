82a482c6ee468bbfeb1b0428bae478133802bd26
Ramenu <abdurpro@gmail.com>
2022-03-19 16:59:01 -0400

    PKI function takes camera vecs now

--- include/minecraft/window.hpp
+++ include/minecraft/window.hpp
@@ -8,7 +8,7 @@ namespace Window
 {
     extern GLFWwindow *loadWindow(GLFWwindow *window, const char &title) noexcept;
     extern void initWindow(const char *windowName) noexcept;
-    extern void processKeyboardInput(double deltaTime, Camera &camera) noexcept; 
+    extern void processKeyboardInput(float deltaTime, glm::vec3 &cameraPos, const glm::vec3 &cameraFront, const glm::vec3 &cameraRight) noexcept; 
     extern void renderWireframes() noexcept;
     extern void destroyWindow() noexcept;
     extern GLFWwindow *getWindow() noexcept;
