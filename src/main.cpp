6370ceab502eca83573c7385c3a447858152c502
Ramenu <abdurpro@gmail.com>
2022-03-18 20:04:59 -0400

    removed game instance

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,14 @@ int main()
     #ifdef _WIN32
         if (enable_virtual_terminal_processing()) // ASCI colors don't work atm ;(
         {
-            Game minecraft {"Minecraft"};
-            minecraft.runGame();
+            initGame("Minecraft");
+            runGame();
         } 
         else
             GLError::error_message("Failed to enable virtual terminal processing");
     #else // ASCI color codes are automatically supported on UNIX systems
-        Game minecraft {"Minecraft"};
-        minecraft.runGame();
+        initGame("Minecraft");
+        runGame();
     #endif
     
     return 0;
