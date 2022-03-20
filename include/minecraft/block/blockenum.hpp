5825a1032d621dfc03cc9d601929cdc74d31aede
Ramenu <abdurpro@gmail.com>
2022-03-20 13:16:24 -0400

    added blocksoundID enum

---
+++ include/minecraft/block/blockenum.hpp
@@ -0,0 +1,16 @@
+#ifndef BLOCKENUM_HPP
+#define BLOCKENUM_HPP
+
+enum class BlockName
+{
+    Grass_Block,
+    Cobblestone_Block
+};
+
+enum BlockSoundID
+{
+    Grass_Block = 0,
+    Cobblestone_Block = 1
+};
+
+#endif // BLOCKENUM_HPP
