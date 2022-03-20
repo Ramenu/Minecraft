0e03b6bd00efe5d49094ae33e15d9555b8fbeef7
Ramenu <abdurpro@gmail.com>
2022-03-20 17:19:01 -0400

    made soundID an 8-bit unsigned enum

--- include/minecraft/block/blockenum.hpp
+++ include/minecraft/block/blockenum.hpp
@@ -1,13 +1,15 @@
 #ifndef BLOCKENUM_HPP
 #define BLOCKENUM_HPP
 
+#include <cstdint>
+
 enum class BlockName
 {
     Grass_Block,
     Cobblestone_Block
 };
 
-enum BlockSoundID
+enum BlockSoundID : uint8_t
 {
     Grass_Block = 0,
     Cobblestone_Block = 1
