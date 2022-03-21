cccd34a9222fa682e8f8d116a881f8196ffefd18
Ramenu <abdurpro@gmail.com>
2022-03-21 14:33:16 -0400

    removed useless buffer functions, updated CB func

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,33 +1,31 @@
 #include "glad/glad.h"
 #include "minecraft/buffer.hpp"
 
+
 namespace Buffer
 {
-    /** 
-     * Loads the data into the buffer passed. 
-     * Should be called during the initialization of a buffer object only. 
-     * Buffer MUST be a vertex buffer.
-     */
-    void createBuffer(BufferData &buffer) noexcept
-    {
-        const int draw = (buffer.isStatic) ? GL_STATIC_DRAW : GL_DYNAMIC_DRAW;
-        glGenBuffers(1, &buffer.buffer);
-        glBindBuffer(GL_ARRAY_BUFFER, buffer.buffer);
-        glBufferData(GL_ARRAY_BUFFER, buffer.size, buffer.data, draw);
-    }
-
     /**
-     * Updates the data of the buffer. Should only be used for dynamic buffer. Function assumes that the buffer
-     * passed is a vertex buffer. No checks are performed to confirm that the buffer is a vertex buffer.
-     */  
-    void updateBuffer(const BufferData &buffer) noexcept
+     * Creates a new vertex buffer with
+     * the given buffer data and configures
+     * the attributes with the stride of each
+     * attribute given. 
+     * NOTE: This function enables all attributes at once.
+     */
+    unsigned int createVertexBuffer(BufferData &buffer, const std::vector<size_t> &strides) noexcept
     {
-        glBindBuffer(GL_ARRAY_BUFFER, buffer.buffer);
-        glBufferData(GL_ARRAY_BUFFER, buffer.size, buffer.data, GL_DYNAMIC_DRAW);
-    }
+        size_t size {}, sum {};
+        for (const auto &i: strides) sum += i;
 
-    void deleteBuffer(uint32_t &buffer) noexcept
-    {
-        glDeleteBuffers(1, &buffer);
+        unsigned int newBuffer {};
+        glGenBuffers(1, &newBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, newBuffer);
+        glBufferData(GL_ARRAY_BUFFER, buffer.size, buffer.data, GL_STATIC_DRAW);
+        for (size_t i {}; i < strides.size(); i++)
+        {
+            glVertexAttribPointer(i, strides[i], GL_FLOAT, GL_FALSE, sum * sizeof(float), reinterpret_cast<void*>(size * sizeof(float)));
+            glEnableVertexAttribArray(i);
+            size += strides[i];
+        }
+        return newBuffer;
     }
 }
