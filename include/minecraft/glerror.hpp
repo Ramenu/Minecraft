814d95ea19f975056c7486157e7294251e0d2ce1
Ramenu <abdurpro@gmail.com>
2022-03-21 19:07:04 -0400

    added debug callback enabler

--- include/minecraft/glerror.hpp
+++ include/minecraft/glerror.hpp
@@ -1,11 +1,16 @@
 #ifndef GLERROR_HPP
 #define GLERROR_HPP
 
+#define MC_DEBUG_BUILD
+
 #include <string_view>
 
 namespace GLError
 {
     extern void error_message(std::string_view error) noexcept;
+    #ifdef MC_DEBUG_BUILD
+        extern void enableGLDebugCallBack();
+    #endif
 }
 
 #endif // GLERROR_HPP
