6252e96e38701e52d503c1f7417d9f5eaedac1c8
Ramenu <abdurpro@gmail.com>
2022-03-22 12:43:02 -0400

    removed includes and shader binary methods

--- src/shader.cpp
+++ src/shader.cpp
@@ -2,10 +2,6 @@
 #include "minecraft/glerror/glerror.hpp"
 #include <fstream>
 #include <sstream>
-#include <vector>
-#include <filesystem>
-
-namespace fs = std::filesystem;
 
 /** 
  * Loads the vertex and fragment shader and links them into 
@@ -16,85 +12,47 @@ namespace fs = std::filesystem;
  */
 Shader::Shader(const char *vertexShaderSource, const char *fragmentShaderSource) noexcept
 {
-    do 
-    {
-        // First check if any precompiled binaries are available for use
-        if (!binaryAvailable(vertexShaderSource))
-        {
-            std::stringstream vertexShaderStream, fragmentShaderStream;
-            std::ifstream vertexShaderFile, fragmentShaderFile;
+    std::stringstream vertexShaderStream, fragmentShaderStream;
+    std::ifstream vertexShaderFile, fragmentShaderFile;
 
-            vertexShaderFile.open(vertexShaderSource);
-            fragmentShaderFile.open(fragmentShaderSource);
+    vertexShaderFile.open(vertexShaderSource);
+    fragmentShaderFile.open(fragmentShaderSource);
 
-            // Read file contents into the string streams
-            vertexShaderStream << vertexShaderFile.rdbuf();
-            fragmentShaderStream << fragmentShaderFile.rdbuf();
+    // Read file contents into the string streams
+    vertexShaderStream << vertexShaderFile.rdbuf();
+    fragmentShaderStream << fragmentShaderFile.rdbuf();
 
-            vertexShaderFile.close();
-            fragmentShaderFile.close();
-            
-            // Compile vertex shader
-            const std::string vertexShaderCode {vertexShaderStream.str()};
-            const char *cstrVertexShaderCode {vertexShaderCode.c_str()};
-            const uint32_t vertexShader {glCreateShader(GL_VERTEX_SHADER)};
-            glShaderSource(vertexShader, 1, &cstrVertexShaderCode, NULL);
-            glCompileShader(vertexShader);
-            checkShaderCompilationErrors(vertexShader, GL_VERTEX_SHADER, vertexShaderSource);
-
-            // Compile fragment shader
-            const std::string fragmentShaderCode {fragmentShaderStream.str()};
-            const char *cstrFragmentShaderCode {fragmentShaderCode.c_str()};
-            const uint32_t fragmentShader {glCreateShader(GL_FRAGMENT_SHADER)};
-            glShaderSource(fragmentShader, 1, &cstrFragmentShaderCode, NULL);
-            glCompileShader(fragmentShader);
-            checkShaderCompilationErrors(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderSource);
-            
-            shaderProgram = glCreateProgram();
-
-            // Attach the shaders together and link the program (and check for errors)
-            glAttachShader(shaderProgram, vertexShader);
-            glAttachShader(shaderProgram, fragmentShader);
-            glLinkProgram(shaderProgram);
-            checkLinkageErrors();
-
-            glDeleteShader(vertexShader); 
-            glDeleteShader(fragmentShader);
-
-            if (canCreateShaderBinary())
-                createShaderBinary(vertexShaderSource);
-        }
-        else
-        {
-            
-        }
-    } while (true);
+    vertexShaderFile.close();
+    fragmentShaderFile.close();
     
+    // Compile vertex shader
+    const std::string vertexShaderCode {vertexShaderStream.str()};
+    const char *cstrVertexShaderCode {vertexShaderCode.c_str()};
+    const uint32_t vertexShader {glCreateShader(GL_VERTEX_SHADER)};
+    glShaderSource(vertexShader, 1, &cstrVertexShaderCode, NULL);
+    glCompileShader(vertexShader);
+    checkShaderCompilationErrors(vertexShader, GL_VERTEX_SHADER, vertexShaderSource);
+
+    // Compile fragment shader
+    const std::string fragmentShaderCode {fragmentShaderStream.str()};
+    const char *cstrFragmentShaderCode {fragmentShaderCode.c_str()};
+    const uint32_t fragmentShader {glCreateShader(GL_FRAGMENT_SHADER)};
+    glShaderSource(fragmentShader, 1, &cstrFragmentShaderCode, NULL);
+    glCompileShader(fragmentShader);
+    checkShaderCompilationErrors(fragmentShader, GL_FRAGMENT_SHADER, fragmentShaderSource);
+    
+    shaderProgram = glCreateProgram();
+
+    // Attach the shaders together and link the program (and check for errors)
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    checkLinkageErrors();
+
+    glDeleteShader(vertexShader); 
+    glDeleteShader(fragmentShader);
 }
 
-bool Shader::canCreateShaderBinary()
-{
-    int formats {};
-    glGetIntegerv(GL_NUM_PROGRAM_BINARY_FORMATS, &formats);
-    return (formats < 1);
-}
-
-void Shader::createShaderBinary(const char *outputFile)
-{
-    int length {};
-    glGetProgramiv(shaderProgram, GL_PROGRAM_BINARY_LENGTH, &length);
-
-    // Get the binary code
-    GLenum format {};
-    std::vector<GLubyte> buffer (length);
-    glGetProgramBinary(shaderProgram, length, NULL, &format, buffer.data());
-
-    // Write binary to a file
-    std::ofstream out {outputFile, std::ios::binary};
-    out.write(reinterpret_cast<char*>(buffer.data(), length));
-    out.close();
-
-}
 
 /**
  * Checks if shader compilation was successful. If not,
