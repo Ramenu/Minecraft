b60102c82c5c7f47d6f028a7d2cd3fc73ba796d3
Ramenu <abdurpro@gmail.com>
2022-04-09 14:35:40 -0400

    Added sound.cpp for sound-handling events

---
+++ src/sound.cpp
@@ -0,0 +1,43 @@
+#include "minecraft/audio/sound.hpp"
+#include "minecraft/audio/sounddata.hpp"
+
+namespace Sound
+{
+    
+    static std::pair<SoundData, SoundData> blockSounds[2] {
+        std::pair<SoundData, SoundData>{"./sfx/grassblock/grassplace.ogg", "./sfx/grassblock/grassbreak.ogg"}, // Grass block sounds
+        std::pair<SoundData, SoundData>{"./sfx/stone/stoneplace.ogg", "./sfx/stone/stonebreak.ogg"} // Stone block sounds
+    };
+
+    /**
+     * Returns the sound ID of the block.
+     * Some blocks may share similar sounds, so
+     * the enum cannot be used directly. That is why
+     * this function exists.
+     */
+    static uint8_t getSoundID(BlockName name)
+    {
+        switch (name)
+        {
+            default: return 0;
+            case BlockName::Cobblestone_Block: return 1;
+            case BlockName::Dirt_Block: return 0;
+        }
+    }
+
+    /**
+     * Plays the block's sound on creation.
+     */
+    void playBlockPlacementSound(BlockName name) 
+    {
+        blockSounds[getSoundID(name)].first.audio.play();
+    }
+    
+    /**
+     * Plays the block's destruction sound.
+     */
+    void playBlockBreakSound(BlockName name) 
+    {
+        blockSounds[getSoundID(name)].second.audio.play();
+    }
+}
