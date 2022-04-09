4e07f005b24436876177efe9d4e6c3796203d33e
Ramenu <abdurpro@gmail.com>
2022-04-09 14:34:40 -0400

    updated atlasHeight

--- include/minecraft/gfx/texture.hpp
+++ include/minecraft/gfx/texture.hpp
@@ -9,7 +9,7 @@ namespace Texture
     extern void loadTexture(const char *texturePath) noexcept;
     [[nodiscard]] extern ImageData loadImage(const char *imagePath) noexcept;
     extern void createTexture(const char *texturePath, uint32_t &texture) noexcept;
-    constexpr uint16_t atlasWidth {3}, atlasHeight {2};
+    constexpr uint16_t atlasWidth {3}, atlasHeight {3};
     constexpr float xPos {1.0f / atlasWidth}, yPos {1.0f / atlasHeight};
     extern uint32_t getTextureAtlas() noexcept;
     extern void initTextureAtlas() noexcept;
