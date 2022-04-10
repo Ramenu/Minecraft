09e2155170aa0e0752b9addc97ae391522576048
Ramenu <abdurpro@gmail.com>
2022-04-09 22:23:45 -0400

    Added getBlockPos function

--- include/minecraft/math/glmath.hpp
+++ include/minecraft/math/glmath.hpp
@@ -6,6 +6,14 @@
 namespace GLMath
 {
     extern glm::vec3 closestDirectionTo(const glm::vec3 &vec) noexcept;
+
+    /**
+     * Returns the position of the block
+     * based on (x, y, z) coordinates passed.
+     */
+    inline glm::vec3 getBlockPos(const glm::vec3 &location) {
+        return glm::vec3{0.5f * location.x, location.y, location.z * 0.5f};
+    }
 }
 
 #endif // GLMATH_HPP
