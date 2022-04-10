0d0a09ded59c4eca8536e21e4b67f9e1a3526a73
Ramenu <abdurpro@gmail.com>
2022-04-09 22:22:56 -0400

    Added chunk.hpp

---
+++ include/minecraft/rendering/chunk.hpp
@@ -0,0 +1,51 @@
+#ifndef CHUNK_HPP
+#define CHUNK_HPP
+
+#include <cstdint>
+#include "minecraft/block/block.hpp"
+#include "glm/vec3.hpp"
+#include <array>
+
+constexpr uint8_t chunkWidth {5}, chunkHeight {5}, chunkLength {5};
+
+class Chunk
+{
+    private:
+        /**
+         * Initializes the chunk with the the name
+         * of the two blocks given. The first parameter
+         * specifies the block that will be used to fill
+         * the top-most layer, and the second name 
+         * is used to fill the bottom layers.
+         * The chunk returned is (chunkWidth x ChunkHeight x chunkLength).
+         */
+        static constexpr std::array<std::array<std::array<Block, chunkWidth>, chunkHeight>, chunkLength> 
+        initChunk(BlockName firstLayer, BlockName beneathFirstLayer) noexcept
+        {
+            std::array<std::array<std::array<Block, chunkWidth>, chunkHeight>, chunkLength> blocks;
+            for (int8_t i {}; i < chunkWidth; i++)
+            {
+                BlockName selectedBlock = firstLayer;
+                for (int8_t j {chunkHeight - 1}; j >= 0; j--)
+                {
+                    for (int8_t k {}; k < chunkLength; k++)
+                        blocks[i][j][k] = Block{selectedBlock};
+                    selectedBlock = beneathFirstLayer;
+                }
+            }
+            return blocks;
+        }
+    public:
+        /**
+         * Returns true if the position/indices 
+         * is out of the chunk array's boundaries.
+         */
+        static inline bool isOutOfChunk(const glm::vec3 &pos) {
+            return (pos.x >= chunkWidth || pos.y >= chunkHeight || pos.z >= chunkLength);
+        }
+        std::array<std::array<std::array<Block, chunkWidth>, chunkHeight>, chunkLength> chunk;
+        constexpr Chunk(BlockName firstLayer, BlockName beneathFirstLayer) : chunk {initChunk(firstLayer, beneathFirstLayer)} {}
+};
+
+
+#endif // CHUNK_HPP
