2f4035d3ffd470407743e1daae6a6a80538e37bd
Ramenu <abdurpro@gmail.com>
2022-04-13 21:58:56 -0400

    Added cubevertex struct and other constants, fns

--- include/minecraft/data/vertices.hpp
+++ include/minecraft/data/vertices.hpp
@@ -1,6 +1,26 @@
 #ifndef VERTICES_HPP
 #define VERTICES_HPP
 
-extern const float cubeVertices[288];
+#include "minecraft/rendering/chunk.hpp"
+#include <vector>
+
+constexpr size_t attributesToFormCube {36}, noOfSquaresInCube {6},
+                 positionAttributeSize {3}, textureAttributeSize {2}, lightAttributeSize {3},
+                 posVerticesSize {positionAttributeSize * noOfSquaresInCube * noOfSquaresInCube},
+                 textureVerticesSize {textureAttributeSize * noOfSquaresInCube * noOfSquaresInCube},
+                 lightDirVerticesSize {lightAttributeSize * noOfSquaresInCube * noOfSquaresInCube},
+                 noOfVertices {posVerticesSize + textureVerticesSize + lightDirVerticesSize};
+
+struct CubeVertex
+{
+    std::vector<float> positionVertices;
+    std::vector<float> textureVertices;
+    std::vector<float> lightDirectionVertices;
+};
+
+
+extern CubeVertex createCubeAt(float x, float y, float z);
+extern std::vector<CubeVertex> initChunkVertices();
+
 
 #endif // VERTICES_HPP
--- src/vertices.cpp
+++ src/vertices.cpp
@@ -1,109 +1,173 @@
 #include "minecraft/data/vertices.hpp"
 #include "minecraft/gfx/texture.hpp"
+#include <iostream>
 
 using namespace Texture;
 
 constexpr float xPos_2 {(atlasWidth - 1) * xPos};
 
 
-// For future reference to the y coordinates, just adjust them inside the vertex shader
-// using the .y attribute
-constexpr float cubeVertices[288] =
+CubeVertex createCubeAt(float x, float y, float z) 
 {
-   // Position          Texture coordinates     Normal direction (Light)
+    CubeVertex cubeVertex;
+    cubeVertex.lightDirectionVertices = {
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
 
-   // Back (First square)
-   -0.5f, -0.5f, 0.0f,    0.0f,      yPos,       0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.0f,    xPos,      0.0f,       0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.0f,    xPos,      yPos,       0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.0f,    xPos,      0.0f,       0.0f, 0.0f,  1.0f,
-   -0.5f, -0.5f, 0.0f,    0.0f,      yPos,       0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.0f,    0.0f,      0.0f,       0.0f, 0.0f,  1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
 
-    // Front (Second square)
-   -0.5f, -0.5f, 0.5f,    0.0f,      yPos,       0.0f, 0.0f,  1.0f,
-    0.0f, -0.5f, 0.5f,    xPos,      yPos,       0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.5f,    xPos,      0.0f,       0.0f, 0.0f,  1.0f,
-    0.0f,  0.5f, 0.5f,    xPos,      0.0f,       0.0f, 0.0f,  1.0f,
-   -0.5f,  0.5f, 0.5f,    0.0f,      0.0f,       0.0f, 0.0f,  1.0f,
-   -0.5f, -0.5f, 0.5f,    0.0f,      yPos,       0.0f, 0.0f,  1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
 
-    // Right (Third square)
-    0.0f,  0.5f, 0.0f,    xPos,      0.0f,       1.0f, 0.0f,  0.0f,
-    0.0f, -0.5f, 0.5f,    0.0f,      yPos,       1.0f, 0.0f,  0.0f,
-    0.0f, -0.5f, 0.0f,    xPos,      yPos,       1.0f, 0.0f,  0.0f,
-    0.0f, -0.5f, 0.5f,    0.0f,      yPos,       1.0f, 0.0f,  0.0f,
-    0.0f,  0.5f, 0.0f,    xPos,      0.0f,       1.0f, 0.0f,  0.0f,
-    0.0f,  0.5f, 0.5f,    0.0f,      0.0f,       1.0f, 0.0f,  0.0f,
- 
-    // Left (Fourth square)
-   -0.5f,  0.5f, 0.0f,	  xPos,      0.0f,       1.0f, 0.0f,  0.0f,
-   -0.5f, -0.5f, 0.0f,    xPos,      yPos,       1.0f, 0.0f,  0.0f,   
-   -0.5f, -0.5f, 0.5f,	  0.0f,      yPos,       1.0f, 0.0f,  0.0f,  
-   -0.5f, -0.5f, 0.5f,	  0.0f,      yPos,       1.0f, 0.0f,  0.0f,  
-   -0.5f,  0.5f, 0.5f,	  0.0f,      0.0f,       1.0f, 0.0f,  0.0f, 
-   -0.5f,  0.5f, 0.0f,	  xPos,      0.0f,       1.0f, 0.0f,  0.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
+        0.0f,  0.0f, 1.0f,
 
-    // Up (fifth square)
-   -0.5f,  0.5f, 0.0f,    xPos,      0.0f,       0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    xPos_2,    yPos,       0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.0f,    xPos_2,    0.0f,       0.0f,  1.0f, 0.0f,
-    0.0f,  0.5f, 0.5f,    xPos_2,    yPos,       0.0f,  1.0f, 0.0f,
-   -0.5f,  0.5f, 0.0f,    xPos,      0.0f,       0.0f,  1.0f, 0.0f,
-   -0.5f,  0.5f, 0.5f,    xPos,      yPos,       0.0f,  1.0f, 0.0f, 
+        0.0f,  1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
 
-    // Bottom (sixth square)
-   -0.5f, -0.5f, 0.5f,    xPos_2,    yPos,       0.0f, -1.0f, 0.0f,   
-   -0.5f, -0.5f, 0.0f,    xPos_2,    0.0f,       0.0f, -1.0f, 0.0f,
-    0.0f, -0.5f, 0.5f,    1.0f,      yPos,       0.0f, -1.0f, 0.0f,                  
-    0.0f, -0.5f, 0.5f,    1.0f,      yPos,       0.0f, -1.0f, 0.0f,                  
-   -0.5f, -0.5f, 0.0f,    xPos_2,    0.0f,       0.0f, -1.0f, 0.0f,               
-    0.0f, -0.5f, 0.0f,    xPos_2,    0.0f,       0.0f, -1.0f, 0.0f              
-};
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f
+    };
 
-/* BACKUP
-constexpr float cubeVertices[288] = {
-    // Back face 
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,     0.0f, 0.0f, 1.0f,  
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,     0.0f, 0.0f, 1.0f,   
-     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,     0.0f, 0.0f, 1.0f,             
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,     0.0f, 0.0f, 1.0f,   
-    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,     0.0f, 0.0f, 1.0f,   
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,     0.0f, 0.0f, 1.0f, 
-    // Front face 
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,     0.0f, 0.0f, 1.0f,  
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,     0.0f, 0.0f, 1.0f,   
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,     0.0f, 0.0f, 1.0f,  
-     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,     0.0f, 0.0f, 1.0f, 
-    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,     0.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,     0.0f, 0.0f, 1.0f, 
-    // Left face
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     1.0f, 0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,     1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     1.0f, 0.0f, 0.0f, 
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     1.0f, 0.0f, 0.0f, 
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,     1.0f, 0.0f, 0.0f,  
-    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     1.0f, 0.0f, 0.0f, 
-    // Right face
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     1.0f, 0.0f, 0.0f,  
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,     1.0f, 0.0f, 0.0f,        
-     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     1.0f, 0.0f, 0.0f,  
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,     1.0f, 0.0f, 0.0f,      
-    // Bottom face
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     0.0f, -1.0f, 0.0f,    
-     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,     0.0f, -1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,     0.0f, -1.0f, 0.0f, 
-     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,     0.0f, -1.0f, 0.0f, 
-    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,     0.0f, -1.0f, 0.0f,  
-    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,     0.0f, -1.0f, 0.0f, 
-    // Top face
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,     0.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     0.0f, 1.0f, 0.0f, 
-     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,     0.0f, 1.0f, 0.0f,   
-     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,     0.0f, 1.0f, 0.0f, 
-    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,     0.0f, 1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,     0.0f, 1.0f, 0.0f,
-};
-*/
+    cubeVertex.textureVertices = {
+        0.0f,   yPos,
+        xPos,   0.0f,
+        xPos,   yPos,
+        xPos,   0.0f,
+        0.0f,   yPos,
+        0.0f,   0.0f,
+
+        0.0f,   yPos,
+        xPos,   yPos,
+        xPos,   0.0f,
+        xPos,   0.0f,
+        0.0f,   0.0f,
+        0.0f,   yPos,
+
+        xPos,   0.0f,
+        0.0f,   yPos,
+        xPos,   yPos,
+        0.0f,   yPos,
+        xPos,   0.0f,
+        0.0f,   0.0f,
+
+        xPos,   0.0f,
+        xPos,   yPos,
+        0.0f,   yPos,
+        0.0f,   yPos,
+        0.0f,   0.0f,
+        xPos,   0.0f,
+
+        xPos,   0.0f,
+        xPos_2, yPos,
+        xPos_2, 0.0f,
+        xPos_2, yPos,
+        xPos,   0.0f,
+        xPos,   yPos,
+
+        xPos_2, yPos,
+        xPos_2, 0.0f,
+        1.0f,   yPos,
+        1.0f,   yPos,
+        xPos_2, 0.0f,
+        xPos_2, 0.0f
+    };
+
+    cubeVertex.positionVertices = {
+        // Back (First square)
+        -0.5f + x, -0.5f + y, 0.0f + z,
+         0.0f + x,  0.5f + y, 0.0f + z,
+         0.0f + x, -0.5f + y, 0.0f + z,
+         0.0f + x,  0.5f + y, 0.0f + z,
+        -0.5f + x, -0.5f + y, 0.0f + z,
+        -0.5f + x,  0.5f + y, 0.0f + z,
+
+        // Front (Second square)
+        -0.5f + x, -0.5f + y, 0.5f + z,
+         0.0f + x, -0.5f + y, 0.5f + z,
+         0.0f + x,  0.5f + y, 0.5f + z,
+         0.0f + x,  0.5f + y, 0.5f + z,
+        -0.5f + x,  0.5f + y, 0.5f + z,
+        -0.5f + x, -0.5f + y, 0.5f + z,
+
+        // Right (third square)
+         0.0f + x,  0.5f + y, 0.0f + z,
+         0.0f + x, -0.5f + y, 0.5f + z,
+         0.0f + x, -0.5f + y, 0.0f + z,
+         0.0f + x, -0.5f + y, 0.5f + z,
+         0.0f + x,  0.5f + y, 0.0f + z,
+         0.0f + x,  0.5f + y, 0.5f + z,
+
+        // Left (Fourth square)
+        -0.5f + x,  0.5f + y, 0.0f + z,
+        -0.5f + x, -0.5f + y, 0.0f + z,
+        -0.5f + x, -0.5f + y, 0.5f + z,
+        -0.5f + x, -0.5f + y, 0.5f + z,
+        -0.5f + x,  0.5f + y, 0.5f + z,
+        -0.5f + x,  0.5f + y, 0.0f + z,
+
+        // Up (fifth square)
+        -0.5f + x,  0.5f + y, 0.0f + z,
+         0.0f + x,  0.5f + y, 0.5f + z,
+         0.0f + x,  0.5f + y, 0.0f + z,
+         0.0f + x,  0.5f + y, 0.5f + z,
+        -0.5f + x,  0.5f + y, 0.0f + z,
+        -0.5f + x,  0.5f + y, 0.5f + z,
+
+        // Down (sixth square)
+        -0.5f + x, -0.5f + y, 0.5f + z,
+        -0.5f + x, -0.5f + y, 0.0f + z,
+         0.0f + x, -0.5f + y, 0.5f + z,
+         0.0f + x, -0.5f + y, 0.5f + z,
+        -0.5f + x, -0.5f + y, 0.0f + z,
+         0.0f + x, -0.5f + y, 0.0f + z
+   };
+
+   return cubeVertex;
+}
+
+std::vector<CubeVertex> initChunkVertices() 
+{
+    std::vector<CubeVertex> chunkVertices;
+
+    // Note that the size of the vector is not the size of the chunkVolume
+    // because x and z are incremented by 0.5f each time, so therefore it is 
+    // actually chunkVolume * 2 * 2.
+    for (float x {}; x < chunkWidth; x += 0.5f)
+    {
+        for (float y {}; y < chunkHeight; y += 1.0f)
+        {
+            for (float z {}; z < chunkLength; z += 0.5f)
+            {
+                chunkVertices.emplace_back(createCubeAt(x, y, z));
+            }
+        }
+    }
+
+    return chunkVertices;
+}
