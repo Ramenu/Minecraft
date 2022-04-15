7e5b3717f2924313f5f430022992a05e9b3cb627
Ramenu <abdurpro@gmail.com>
2022-04-14 22:03:23 -0400

    Removed class instances, and modified constructor

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -14,7 +14,6 @@ static constexpr float strideToNextBlock {0.5f};
  * vertex buffer data.
  */
 Renderer::Renderer() noexcept : 
-playerCamera {CameraSettings{.yaw = 90.0f, .pitch = 0.0f, .sensitivity = 0.1f}},
 cubeShader {"shaders/block/blockvertexshader.vert", "shaders/block/blockfragmentshader.frag"},
 lightSource {[this]() {
     constexpr LightComponents components {.ambient = {0.25f, 0.25f, 0.25f}, 
@@ -31,63 +30,50 @@ lightSource {[this]() {
     glGenVertexArrays(1, &vertexArray);
     glBindVertexArray(vertexArray);
 
+    chunk.modifyChunk(15, 15, 15, Block{BlockName::Dirt_Block}, {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f});
     // Create vertex buffer
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    
+    const size_t chunkPosSize {chunk.getVertices().position.size() * sizeof(float)},
+               chunkTexSize {chunk.getVertices().texture.size() * sizeof(float)},
+               chunkLightDirSize {chunk.getVertices().lightDirection.size() * sizeof(float)},
+               chunkVisibilitySize {chunk.getVertices().visibility.size() * sizeof(float)};
+    
+    const size_t verticesBytes {chunkPosSize + chunkTexSize + chunkLightDirSize + chunkVisibilitySize};
 
-    std::vector<CubeVertex> chunkVertices {initChunkVertices()};
-    CubeVertex cube {chunkVertices[0]};
-    for (size_t i {1}; i < chunkVertices.size(); i++)
-    {
-        cube.positionVertices.insert(cube.positionVertices.end(), 
-                                     chunkVertices[i].positionVertices.begin(), 
-                                     chunkVertices[i].positionVertices.end());
-        
-        cube.textureVertices.insert(cube.textureVertices.end(),
-                                    chunkVertices[i].textureVertices.begin(),
-                                    chunkVertices[i].textureVertices.end());
-        
-        cube.lightDirectionVertices.insert(cube.lightDirectionVertices.end(),
-                                    chunkVertices[i].lightDirectionVertices.begin(),
-                                    chunkVertices[i].lightDirectionVertices.end());
-    }
-    printf("Size of chunk vertices is: %lld\n", chunkVertices.size());
-
-    constexpr size_t verticesBytes {noOfVertices * sizeof(float)};
-
+    // First allocate enough data for the buffer to store
     constexpr auto vertexData = nullptr;
-    const size_t chunkPosSize {posVerticesSize * sizeof(float) * chunkVertices.size()},
-               chunkTexSize {textureVerticesSize * sizeof(float) * chunkVertices.size()},
-               chunkLightDirSize {lightDirVerticesSize * sizeof(float) * chunkVertices.size()};
+    glBufferData(GL_ARRAY_BUFFER, verticesBytes * 500, vertexData, GL_DYNAMIC_DRAW);
 
-    glBufferData(GL_ARRAY_BUFFER, verticesBytes * chunkVertices.size(), vertexData, GL_DYNAMIC_DRAW);
-    glBufferSubData(GL_ARRAY_BUFFER, 0, chunkPosSize, &cube.positionVertices[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize, chunkTexSize, &cube.textureVertices[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize, chunkLightDirSize, &cube.lightDirectionVertices[0]);
+    // Now fill in the buffer's data
+    glBufferSubData(GL_ARRAY_BUFFER, 0, chunkPosSize, &chunk.getVertices().position[0]);
+    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize, chunkTexSize, &chunk.getVertices().texture[0]);
+    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize, chunkLightDirSize, &chunk.getVertices().lightDirection[0]);
+    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize + chunkLightDirSize, chunkVisibilitySize, 
+                    &chunk.getVertices().visibility[0]);
 
     constexpr auto isNormalized = GL_FALSE;
     glEnableVertexArrayAttrib(vertexBuffer, 0);
     glVertexAttribPointer(0, positionAttributeSize, GL_FLOAT, isNormalized, 
-                          3 * sizeof(float), reinterpret_cast<const void*>(0));
+                          positionAttributeSize * sizeof(float), reinterpret_cast<const void*>(0));
 
     glEnableVertexArrayAttrib(vertexBuffer, 1);
     glVertexAttribPointer(1, textureAttributeSize, GL_FLOAT, isNormalized, 
-                          2 * sizeof(float), reinterpret_cast<const void*>(chunkPosSize));
+                          textureAttributeSize * sizeof(float), reinterpret_cast<const void*>(chunkPosSize));
 
     glEnableVertexArrayAttrib(vertexBuffer, 2);
     glVertexAttribPointer(2, lightAttributeSize, GL_FLOAT, isNormalized, 
-                          3 * sizeof(float), reinterpret_cast<const void*>(chunkPosSize + chunkTexSize));
-    
+                          lightAttributeSize * sizeof(float), reinterpret_cast<const void*>(chunkPosSize + chunkTexSize));
 
-    glm::mat3 normalMatrix {glm::transpose(glm::inverse(glm::mat4(1.0f)))}; // No idea what this does yet..
+    glEnableVertexArrayAttrib(vertexBuffer, 3);
+    glVertexAttribPointer(3, visibleAttributeSize, GL_FLOAT, isNormalized, visibleAttributeSize * sizeof(float),
+                          reinterpret_cast<const void*>(chunkPosSize + chunkTexSize + chunkLightDirSize));
+    
     cubeShader.useShader(); 
-    cubeShader.setMat3("normalMatrix", normalMatrix);
     lightSource.shaderProgramLightSource(cubeShader);
 
     cubeShader.setInt("allTextures", 0);
-    // Bind the uniform buffer and fill the data with the projection matrix
-    glBindBuffer(GL_UNIFORM_BUFFER, uniformBuffer);
-    glBufferSubData(GL_UNIFORM_BUFFER, 0, sizeof(glm::mat4), &projection[0][0]);
 
     // Same process again... for the light cubeShader (Don't need this right now since it is a directional light at the moment)
     #if 0
@@ -123,32 +109,25 @@ void Renderer::drawLightSource() noexcept
     // glDrawArrays(GL_TRIANGLES, 0, 36);
 }
 
-/**
- * Updates the camera view for the player, 
- * and updates all the shaders to the updated 
- * position of the camera. 
- */
-void Renderer::updateView() noexcept
+
+void Renderer::update() noexcept
 {
-    double xPos, yPos;
-    glfwGetCursorPos(Window::window, &xPos, &yPos);
-    playerCamera.updateCameraPos(xPos, yPos);
-    cubeShader.setMat4("view", playerCamera.getView());
-    cubeShader.setVec3("viewPos", playerCamera.cameraPos);
-    #if 0
-        lightSource->lightShader.useShader();
-        lightSource->lightShader.setMat4("view", playerCamera.view);
-    #endif
+
 }
 
+/**
+ * Draws all of the chunks.
+ */
 void Renderer::draw() const noexcept 
 {
+    constexpr float ambient {1.2f};
     glm::mat4 model {1.0f};
     glm::vec3 blockPos {0.0f, 0.0f, 0.0f};
     glm::translate(model, blockPos);
     cubeShader.useShader();
     cubeShader.setMat4("model", model);
+    cubeShader.setFloat("material.ambient", ambient);
 
-    glDrawArrays(GL_TRIANGLES, 0, attributesToFormCube * chunkVolume * 2 * 2);
+    glDrawArrays(GL_TRIANGLES, 0, chunkVolume * attributesToFormCube);
 }
 
