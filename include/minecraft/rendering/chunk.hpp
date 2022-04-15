55430fc692a179345e748571e4ea47cfac8004c7
Ramenu <abdurpro@gmail.com>
2022-04-15 16:57:09 -0400

    Added some of the older chunk methods back

--- include/minecraft/rendering/chunk.hpp
+++ include/minecraft/rendering/chunk.hpp
@@ -14,12 +14,24 @@ class Chunk
 {
     private:
         ChunkVertex chunkVertices;
+        void updateChunkVisibility() noexcept;
     public:
         Chunk(BlockName firstLayer, BlockName bottomLayers) noexcept;
         void modifyChunk(uint8_t x, uint8_t y, uint8_t z,
                          Block block,
                          const std::array<float, noOfSquaresInCube> &visible) noexcept;
         inline ChunkVertex getVertices() const {return chunkVertices;}
+        bool blockIsVisibleToPlayer(const glm::vec3 &block3DIndex) const noexcept;
+        /**
+         * This function requires a 3D vector with 3 indices
+         * to access the block. Returns true if any of the indices
+         * are out of the chunk's array boundaries.
+         */
+        static inline bool isOutOfChunk(const glm::vec3 &pos) {
+            return ((pos.x < 0.0f || pos.y < 0.0f || pos.z < 0.0f) ||
+                   (pos.x >= chunkWidth || pos.y >= chunkHeight || pos.z >= chunkLength));
+        }
+
         std::array<std::array<std::array<Block, chunkLength>, chunkHeight>, chunkWidth> chunk;
         
 
