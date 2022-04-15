db60cdc49b3c50eab1dee0dcc8cef7504e643848
Ramenu <abdurpro@gmail.com>
2022-04-15 13:50:23 -0400

    Modified constructor and chunkvolume to 32 bit int

--- include/minecraft/rendering/chunk.hpp
+++ include/minecraft/rendering/chunk.hpp
@@ -8,18 +8,19 @@
 #include "minecraft/data/vertices.hpp"
 
 constexpr uint8_t chunkWidth {16}, chunkHeight {16}, chunkLength {16};
-constexpr size_t chunkVolume {chunkWidth * chunkHeight * chunkLength};
+constexpr uint32_t chunkVolume {chunkWidth * chunkHeight * chunkLength};
 
 class Chunk
 {
     private:
         ChunkVertex chunkVertices;
     public:
-        Chunk() noexcept;
+        Chunk(BlockName firstLayer, BlockName bottomLayers) noexcept;
         void modifyChunk(uint8_t x, uint8_t y, uint8_t z,
                          Block block,
                          const std::array<float, noOfSquaresInCube> &visible) noexcept;
         inline ChunkVertex getVertices() const {return chunkVertices;}
+        std::array<std::array<std::array<Block, chunkLength>, chunkHeight>, chunkWidth> chunk;
         
 
 };
