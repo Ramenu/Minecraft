5a67de65b54da0adf8c5eb5a90706a9c010fb51b
Ramenu <abdurpro@gmail.com>
2022-04-19 13:32:23 -0400

    Removed camera class and made it a namespace

--- include/minecraft/camera/camera.hpp
+++ include/minecraft/camera/camera.hpp
@@ -5,8 +5,8 @@
 #include "glm/gtc/matrix_transform.hpp"
 #include "minecraft/rendering/chunk.hpp"
 
-constexpr float speed {4.5f};
-constexpr glm::vec3 up {0.0f, 1.0f, 0.0f};
+static constexpr float speed {4.5f};
+static constexpr glm::vec3 up {0.0f, 1.0f, 0.0f};
 
 struct CameraSettings
 {
@@ -21,22 +21,15 @@ struct CameraDirections
 	glm::vec3 right {glm::normalize(glm::cross(front, up)) * speed};
 };
 
-class Camera
+namespace Camera
 {
-    public:
-		Camera() = default;
-    	constexpr explicit Camera(const CameraSettings &cameraSettings) noexcept : settings {cameraSettings} {}
-		~Camera() = default;
-		CameraSettings settings;
-		glm::vec3 cameraPos {0.0f, chunkHeight * 2, 3.0f};
-		void updateCameraPos(double cursorX, double cursorY) noexcept;
-		inline glm::mat4 getView() const {return view;}
-		CameraDirections direction;
-	private:
-		glm::mat4 view;
-	public:
-		Ray ray {cameraPos, direction.front};
-	
-};
+	extern glm::mat4 getView() noexcept;
+	extern void updateCameraPos(double cursorX, double cursorY) noexcept;
+	inline CameraSettings settings {.yaw = 90.0f, .pitch = 0.0f, .sensitivity = 0.1f};
+	inline CameraDirections direction;
+	inline glm::vec3 cameraPos {0.0f, chunkHeight * 2, 3.0f};
+	inline Ray ray {cameraPos, direction.front};
+
+}
 
 #endif // CAMERA_HPP
--- src/camera.cpp
+++ src/camera.cpp
@@ -1,66 +1,71 @@
 #include "minecraft/camera/camera.hpp"
 
-
-/** 
- * Updates the position of where the camera is looking.
- * Parameters take the position of the mouse's 
- * x-position and y-position.
- */
-void Camera::updateCameraPos(const double cursorX, const double cursorY) noexcept
+namespace Camera
 {
-	const float cosPitch {cosf(glm::radians(settings.pitch))};
+    static glm::mat4 view;
+    glm::mat4 getView() noexcept {return view;}
 
-    // Calculate a 3D vector given the yaw and pitch
-	glm::vec3 updatedDirection {
-        cosf(glm::radians(settings.yaw)) * cosPitch,
-        sinf(glm::radians(settings.pitch)),
-        sinf(glm::radians(settings.yaw)) * cosPitch
-    };
-
-	view = glm::lookAt(cameraPos, cameraPos + direction.front, up);
-	direction.right = glm::normalize(glm::cross(direction.front, up));
-	direction.front = glm::normalize(updatedDirection);
-
-    static double lastX {}, lastY {};
-    static bool firstMouseMovement {true};
-
-    if (firstMouseMovement)
+    /** 
+     * Updates the position of where the camera is looking.
+     * Parameters take the position of the mouse's 
+     * x-position and y-position.
+     */
+    void updateCameraPos(const double cursorX, const double cursorY) noexcept
     {
+        const float cosPitch {cosf(glm::radians(settings.pitch))};
+
+        // Calculate a 3D vector given the yaw and pitch
+        glm::vec3 updatedDirection {
+            cosf(glm::radians(settings.yaw)) * cosPitch,
+            sinf(glm::radians(settings.pitch)),
+            sinf(glm::radians(settings.yaw)) * cosPitch
+        };
+
+        view = glm::lookAt(cameraPos, cameraPos + direction.front, up);
+        direction.right = glm::normalize(glm::cross(direction.front, up));
+        direction.front = glm::normalize(updatedDirection);
+
+        static double lastX {}, lastY {};
+        static bool firstMouseMovement {true};
+
+        if (firstMouseMovement)
+        {
+            lastX = cursorX;
+            lastY = cursorY;
+            firstMouseMovement = false;
+        }
+
+        const float xOffset {static_cast<float>((cursorX - lastX)) * settings.sensitivity};
+        const float yOffset {static_cast<float>((lastY - cursorY)) * settings.sensitivity}; // y-axis is reversed, which is why the order is flipped
+
+        // Update the last mouse positions to the current position
         lastX = cursorX;
         lastY = cursorY;
-        firstMouseMovement = false;
+
+        settings.yaw += xOffset;
+        settings.pitch += yOffset;
+
+        #if 1
+            // Angles in degrees
+            static constexpr float upDownTurnAngle {88.0f}; 
+            static constexpr float sideTurnAngle {360.0f};
+
+            // Prevent the player tilting their head backwards
+            if (settings.pitch > upDownTurnAngle)
+                settings.pitch = upDownTurnAngle;
+            else if (settings.pitch < -upDownTurnAngle)
+                settings.pitch = -upDownTurnAngle;
+            
+            // Reset the yaw so the player can spin their camera around 
+            if (settings.yaw > sideTurnAngle)
+                settings.yaw = 0.0f;
+            else if (settings.yaw < -sideTurnAngle)
+                settings.yaw = 0.0f;
+        #endif
+
+        #if 1
+            ray.updateRay(cameraPos, direction.front);
+        #endif
+
     }
-
-    const float xOffset {static_cast<float>((cursorX - lastX)) * settings.sensitivity};
-    const float yOffset {static_cast<float>((lastY - cursorY)) * settings.sensitivity}; // y-axis is reversed, which is why the order is flipped
-
-    // Update the last mouse positions to the current position
-    lastX = cursorX;
-    lastY = cursorY;
-
-    settings.yaw += xOffset;
-    settings.pitch += yOffset;
-
-    #if 1
-        // Angles in degrees
-        static constexpr float upDownTurnAngle {88.0f}; 
-        static constexpr float sideTurnAngle {360.0f};
-
-        // Prevent the player tilting their head backwards
-        if (settings.pitch > upDownTurnAngle)
-            settings.pitch = upDownTurnAngle;
-        else if (settings.pitch < -upDownTurnAngle)
-            settings.pitch = -upDownTurnAngle;
-        
-        // Reset the yaw so the player can spin their camera around 
-        if (settings.yaw > sideTurnAngle)
-            settings.yaw = 0.0f;
-        else if (settings.yaw < -sideTurnAngle)
-            settings.yaw = 0.0f;
-    #endif
-
-    #if 1
-        ray.updateRay(cameraPos, direction.front);
-    #endif
-
 }
