2754ba402b32a5a0392bc22a0a4eec21afa39a35
Ramenu <abdurpro@gmail.com>
2022-04-20 18:48:25 -0400

    Added ambient attribute

--- include/minecraft/data/vertices.hpp
+++ include/minecraft/data/vertices.hpp
@@ -12,39 +12,91 @@ enum Attribute
     Position = 0,
     TexCoord = 1,
     LightDirection = 2,
-    Visibility = 3
+    Visibility = 3,
+    Ambient = 4
 };
 
-static constexpr size_t totalAttributes {4};
 
 static constexpr size_t attributesToFormCube {36};
 
 static constexpr size_t noOfSquaresInCube {6};
 
 
-static constexpr size_t attributes[totalAttributes] {
+constexpr std::array attributes {
     3, // Position 
     2, // Texture
     3, // Light direction
-    1  // Visibility
+    1, // Visibility
+    1
 };
 
-static constexpr size_t posVerticesSize {attributes[Attribute::Position] * noOfSquaresInCube * noOfSquaresInCube},
-                 textureVerticesSize {attributes[Attribute::TexCoord] * noOfSquaresInCube * noOfSquaresInCube},
-                 lightDirVerticesSize {attributes[Attribute::LightDirection] * noOfSquaresInCube * noOfSquaresInCube},
-                 visibleVerticesSize {attributes[Attribute::Visibility] * noOfSquaresInCube * noOfSquaresInCube},
-                 noOfVertices {posVerticesSize + textureVerticesSize + lightDirVerticesSize + visibleVerticesSize};
+static constexpr std::array<size_t, attributes.size()> verticesSizes {[]() consteval {
+    std::array<size_t, attributes.size()> ver {};
+    for (size_t i {}; i < ver.size(); i++)
+        ver[i] = attributes[i] * noOfSquaresInCube * noOfSquaresInCube;
+    return ver;
+}()};
+
+static constexpr size_t noOfVertices {[]() consteval {
+    size_t sum {};
+    for (const auto&i: verticesSizes)
+        sum += i;
+    return sum;
+}()};
 
 static constexpr float xPos_2 {(atlasWidth - 1) * xPos};
 
 struct ChunkVertex
 {
-    std::array<std::vector<float>, totalAttributes> attributes; 
+    std::array<std::vector<float>, attributes.size()> attributes; 
 };
 
-constexpr std::array<float, posVerticesSize> createCubeAt(float x, float y, float z)
+
+consteval std::array<float, verticesSizes[Attribute::Ambient]> getAmbientVertices(float ambient)
 {
-    const std::array<float, posVerticesSize> defaultPositionVertices {
+    return {
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+        ambient,
+    };
+}
+
+constexpr std::array<float, verticesSizes[Attribute::Position]> createCubeAt(float x, float y, float z)
+{
+    const std::array<float, verticesSizes[Attribute::Position]> defaultPositionVertices {
         // Back (First square)
         -0.5f + x, -0.5f + y, 0.0f + z,
          0.0f + x,  0.5f + y, 0.0f + z,
@@ -97,9 +149,9 @@ constexpr std::array<float, posVerticesSize> createCubeAt(float x, float y, floa
     return defaultPositionVertices;
 }
 
-constexpr std::array<float, textureVerticesSize> getTextureVertices(float textureID)
+constexpr std::array<float, verticesSizes[Attribute::TexCoord]> getTextureVertices(float textureID)
 {
-    std::array<float, textureVerticesSize> textureVertices {
+    std::array<float, verticesSizes[Attribute::TexCoord]> textureVertices {
         0.0f,   yPos + textureID,
         xPos,   0.0f + textureID,
         xPos,   yPos + textureID,
@@ -147,7 +199,7 @@ constexpr std::array<float, textureVerticesSize> getTextureVertices(float textur
 
 }
 
-static constexpr std::array<float, lightDirVerticesSize> defaultLightDirectionVertices {
+static constexpr std::array<float, verticesSizes[Attribute::LightDirection]> defaultLightDirectionVertices {
     0.0f,  0.0f, 1.0f,
     0.0f,  0.0f, 1.0f,
     0.0f,  0.0f, 1.0f,
@@ -191,9 +243,10 @@ static constexpr std::array<float, lightDirVerticesSize> defaultLightDirectionVe
     0.0f, -1.0f, 0.0f
 };
 
-constexpr std::array<float, visibleVerticesSize> getVisibleBlockVertices(const std::array<float, noOfSquaresInCube> &visible)
+constexpr std::array<float, verticesSizes[Attribute::Visibility]> 
+getVisibleBlockVertices(const std::array<float, noOfSquaresInCube> &visible)
 {
-    std::array<float, visibleVerticesSize> visibleVertices {
+    std::array<float, verticesSizes[Attribute::Visibility]> visibleVertices {
         1.0f * visible[0],
         1.0f * visible[0],
         1.0f * visible[0],
@@ -240,56 +293,5 @@ constexpr std::array<float, visibleVerticesSize> getVisibleBlockVertices(const s
 
 }
 
-
-
-[[maybe_unused]] static constexpr std::array<float, posVerticesSize> defaultPositionVertices {
-        // Back (First square)
-        -0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
-         0.0f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f,  0.5f, 0.0f,
-
-        // Fr(second,
-        -0.5f, -0.5f, 0.5f,
-         0.0f, -0.5f, 0.5f,
-         0.0f,  0.5f, 0.5f,
-         0.0f,  0.5f, 0.5f,
-        -0.5f,  0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f,
-
-        // Ri(thir,
-         0.0f,  0.5f, 0.0f,
-         0.0f, -0.5f, 0.5f,
-         0.0f, -0.5f, 0.0f,
-         0.0f, -0.5f, 0.5f,
-         0.0f,  0.5f, 0.0f,
-         0.0f,  0.5f, 0.5f,
-
-        // Lequare (th sq,
-        -0.5f,  0.5f, 0.0f,
-        -0.5f, -0.5f, 0.0f,
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, -0.5f, 0.5f,
-        -0.5f,  0.5f, 0.5f,
-        -0.5f,  0.5f, 0.0f,
-
-        // Touare (f sq,
-        -0.5f,  0.5f, 0.0f,
-         0.0f,  0.5f, 0.5f,
-         0.0f,  0.5f, 0.0f,
-         0.0f,  0.5f, 0.5f,
-        -0.5f,  0.5f, 0.0f,
-        -0.5f,  0.5f, 0.5f,
-
-        // Bo squarexth sq,
-        -0.5f, -0.5f, 0.5f,
-        -0.5f, -0.5f, 0.0f,
-         0.0f, -0.5f, 0.5f,
-         0.0f, -0.5f, 0.5f,
-        -0.5f, -0.5f, 0.0f,
-         0.0f, -0.5f, 0.0f
-    };
 
 #endif // VERTICES_HPP
