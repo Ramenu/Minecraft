5dba29ecd4be759a9cae4d9b12c7b62ba272ff26
Ramenu <abdurpro@gmail.com>
2022-04-20 14:24:54 -0400

    removed vbo/vao initialization

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -30,45 +30,10 @@ lightSource {[this]() {
     static constexpr glm::vec3 position {1.0f, 3.0f, 1.0f};
     return Lighting{components, direction, position};
 }()},
-chunk {BlockName::Grass_Block, BlockName::Dirt_Block}
+activeChunk {BlockName::Grass_Block, BlockName::Dirt_Block}
 {
     
-    // Bind our vertex array first
-    vertexArray.bind();
-
-    static constexpr auto blockPos {ChunkIndex{5, 15, 12}};
-    chunk.modifyChunk(blockPos, Block{BlockName::Air_Block});
-
-    // Now bind vertex buffer
-    vertexBuffer.bind();
-
-    // First allocate enough data for the buffer to store
-    static constexpr auto vertexData = nullptr;
-    glBufferData(GL_ARRAY_BUFFER, verticesBytes, vertexData, GL_DYNAMIC_DRAW);
-
-    // Now fill in the buffer's data
-    glBufferSubData(GL_ARRAY_BUFFER, 0, chunkPosSize, &chunk.getVertices().position[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize, chunkTexSize, &chunk.getVertices().texture[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize, chunkLightDirSize, &chunk.getVertices().lightDirection[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize + chunkLightDirSize, chunkVisibilitySize, 
-                    &chunk.getVertices().visibility[0]);
-
-    static constexpr auto isNormalized = GL_FALSE;
-    glEnableVertexArrayAttrib(vertexBuffer.getBuffer(), 0);
-    glVertexAttribPointer(0, positionAttributeSize, GL_FLOAT, isNormalized, 
-                          positionAttributeSize * sizeof(float), nullptr);
-
-    glEnableVertexArrayAttrib(vertexBuffer.getBuffer(), 1);
-    glVertexAttribPointer(1, textureAttributeSize, GL_FLOAT, isNormalized, 
-                          textureAttributeSize * sizeof(float), reinterpret_cast<const void*>(chunkPosSize));
-
-    glEnableVertexArrayAttrib(vertexBuffer.getBuffer(), 2);
-    glVertexAttribPointer(2, lightAttributeSize, GL_FLOAT, isNormalized, 
-                          lightAttributeSize * sizeof(float), reinterpret_cast<const void*>(chunkPosSize + chunkTexSize));
-
-    glEnableVertexArrayAttrib(vertexBuffer.getBuffer(), 3); 
-    glVertexAttribPointer(3, visibleAttributeSize, GL_FLOAT, isNormalized, visibleAttributeSize * sizeof(float),
-                          reinterpret_cast<const void*>(chunkPosSize + chunkTexSize + chunkLightDirSize));
+   
     
     cubeShader.useShader(); 
     lightSource.shaderProgramLightSource(cubeShader);
@@ -98,11 +63,7 @@ Renderer::~Renderer() noexcept
  */
 void Renderer::update() noexcept
 {
-    glBufferSubData(GL_ARRAY_BUFFER, 0, chunkPosSize, &chunk.getVertices().position[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize, chunkTexSize, &chunk.getVertices().texture[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize, chunkLightDirSize, &chunk.getVertices().lightDirection[0]);
-    glBufferSubData(GL_ARRAY_BUFFER, chunkPosSize + chunkTexSize + chunkLightDirSize, chunkVisibilitySize, 
-                    &chunk.getVertices().visibility[0]);
+    activeChunk.update();
 }
 
 /**
