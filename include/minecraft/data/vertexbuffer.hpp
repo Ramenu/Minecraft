484c46a1fe167c2bccd033cd81e75c17aff4a8f0
Ramenu <abdurpro@gmail.com>
2022-04-20 14:19:36 -0400

    changed VBO unsigned int to GLuint

--- include/minecraft/data/vertexbuffer.hpp
+++ include/minecraft/data/vertexbuffer.hpp
@@ -6,19 +6,22 @@
 class VertexBuffer
 {
     private:
-        unsigned int vertexBuffer;
+        GLuint vertexBuffer;
     public:
+
         inline VertexBuffer() noexcept {
             glGenBuffers(1, &vertexBuffer);
         }
+
         inline ~VertexBuffer() noexcept {
             glDeleteBuffers(1, &vertexBuffer);
         }
+
         inline void bind() const noexcept {
             glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
         }
 
-        inline unsigned int getBuffer() const noexcept {
+        inline GLuint getBuffer() const noexcept {
             return vertexBuffer;
         }
 
