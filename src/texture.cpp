c8ec49854cebc0b57c907a619aa23b792bbbcc8d
Ramenu <abdurpro@gmail.com>
2022-04-27 18:39:22 -0400

    added texture class

--- src/texture.cpp
+++ src/texture.cpp
@@ -14,29 +14,16 @@
 
 namespace fs = std::filesystem;
 
-namespace Texture
+
+/**
+ * Creates a texture from the filepath given with 
+ * the default wrapping and filtering configurations. 
+ */
+Texture::Texture(const std::string &pathToTexture) noexcept
 {
-    static uint32_t textureAtlas;
-
-    uint32_t getTextureAtlas() noexcept {return textureAtlas;}
-
-    void initTextureAtlas() noexcept
+    if (fs::exists(pathToTexture))
     {
-        glGenTextures(1, &textureAtlas);
-        createTexture("textures/textureatlas.jpg", textureAtlas);
-    }
-
-    void deleteTextureAtlas() noexcept
-    {
-        glDeleteTextures(1, &textureAtlas);
-    }
-
-    /**
-     * Creates a texture from the filepath given with 
-     * the default wrapping and filtering configurations. 
-     */
-    void createTexture(const char *filePath, uint32_t &texture) noexcept
-    {
+        glGenTextures(1, &texture);
         glBindTexture(GL_TEXTURE_2D, texture);
 
         // Set the texture wrapping parameters
@@ -47,44 +34,24 @@ namespace Texture
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-        loadTexture(filePath); 
-    }
+        int width, height, colorChannels;
+        unsigned char *data {stbi_load(pathToTexture.c_str(), &width, &height, &colorChannels, 0)};
 
-    /**
-     * Loads the texture from the file path given.
-     * If unsuccessful, the program will be terminated.
-     */
-    void loadTexture(const char *texturePath) noexcept
-    {
-        const ImageData img {loadImage(texturePath)};
-        if (fs::path(texturePath).extension() == ".jpg")
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.width, img.height, 0, GL_RGB, GL_UNSIGNED_BYTE, img.data); 
-        else
-            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE,img.data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-
-    /** 
-     * Returns the data of the image, if successful. Otherwise, will terminate
-     * the program. This function has various checks to make sure the path passed
-     * is valid (e.g. making sure it exists and making sure it is a supported image format).
-     */
-    [[nodiscard]] ImageData loadImage(const char *imagePath) noexcept 
-    {
-        if (fs::exists(imagePath))
+        if (data)
         {
-            const std::string extension {fs::path(imagePath).extension().string()};
-            if (extension == ".jpg" || extension == ".png")
-            {
-                ImageData image {};
-                image.data = stbi_load(imagePath, &image.width, &image.height, &image.colorChannels, 0);
-                if (image.data != nullptr)
-                    return image;
-                GLError::error_message("Failed to load data from image");
-            }
-            GLError::error_message("File format must be .png or .jpg");
+            static constexpr int mipmapLevel {0};
+            const auto format {(pathToTexture == ".png") ? GL_RGBA : GL_RGB};
+            glTexImage2D(GL_TEXTURE_2D, mipmapLevel, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+            stbi_image_free(data);
+            return;
         }
-        GLError::error_message(std::string{"No file \"" + std::string{imagePath} + "\" could be found. Double check to make sure it exists.\n"}.c_str());
-        exit(EXIT_FAILURE); // So the compiler doesn't complain about a return missing
+        GLError::error_message("Failed to load data from \"" + pathToTexture + '\"');
     }
+    GLError::error_message("File \"" + pathToTexture + "\" could not be found");
+}
+
+Texture::~Texture() noexcept
+{
+    glDeleteTextures(1, &texture);
 }
