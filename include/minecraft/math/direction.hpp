2f842ad295fad46c492f4bcfe8c7f6bf875ea6d9
Ramenu <abdurpro@gmail.com>
2022-04-28 17:07:30 -0400

    Added direction.hpp

---
+++ include/minecraft/math/direction.hpp
@@ -0,0 +1,29 @@
+#ifndef DIRECTION_HPP
+#define DIRECTION_HPP
+
+#include "glm/vec3.hpp"
+
+constexpr glm::vec3 directions[6] {
+    {1.0f, 0.0f, 0.0f},
+    {-1.0f, 0.0f, 0.0f},
+    {0.0f, 1.0f, 0.0f},
+    {0.0f, -1.0f, 0.0f},
+    {0.0f, 0.0f, 1.0f},
+    {0.0f, 0.0f, -1.0f}
+};
+
+enum Direction : uint8_t
+{
+    East,
+    West,
+    Up,
+    Down,
+    North,
+    South
+};
+
+inline constexpr glm::vec3 getDirectionVector(Direction direction) noexcept {
+    return directions[static_cast<uint8_t>(direction)];
+}
+
+#endif // DIRECTION_HPP
