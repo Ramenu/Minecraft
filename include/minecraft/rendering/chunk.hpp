548b549dd7780e8eb033ca7205e17e7931fbb60d
Ramenu <abdurpro@gmail.com>
2022-04-30 21:44:17 -0400

    removed  chunkindex struct, revamped chunk class

--- include/minecraft/rendering/chunk.hpp
+++ include/minecraft/rendering/chunk.hpp
@@ -5,66 +5,62 @@
 #include "minecraft/block/block.hpp"
 #include "minecraft/data/vertexbuffer.hpp"
 #include "minecraft/data/vertices.hpp"
+#include "minecraft/block/face.h"
+#include <span>
+#include <optional>
 
-constexpr uint8_t chunkWidth {16}, chunkHeight {16}, chunkLength {16};
-constexpr uint32_t chunkVolume {chunkWidth * chunkHeight * chunkLength};
+static constexpr uint8_t chunkWidth {16}, chunkHeight {16}, chunkLength {16};
+static constexpr uint32_t chunkVolume {chunkWidth * chunkHeight * chunkLength};
 static_assert(chunkHeight == chunkWidth && chunkHeight == chunkLength, "ERROR: Width, height, and length of the chunk must be equal!");
 
-struct ChunkIndex
-{
-    int8_t x;
-    int8_t y;
-    int8_t z;
-    inline constexpr ChunkIndex operator+(int8_t num) const noexcept
-    {
-        const int8_t newX = x + num,
-                     newY = y + num,
-                     newZ = z + num;
-        return ChunkIndex{newX, newY, newZ};
-    }
-    inline constexpr ChunkIndex operator-(int8_t num) const noexcept
-    {
-        const int8_t newX = x - num,
-                     newY = y - num,
-                     newZ = z - num;
-        return ChunkIndex{newX, newY, newZ};
-    }
-};
 
 class Chunk
 {
     private:
-        VertexBuffer vertexBuffer;
-        VertexArray vertexArray;
-        ChunkVertex chunkVertices;
-        std::array<float, noOfSquaresInCube> getVisibleFaces(ChunkIndex index) const noexcept;
+        GLuint vertexArray;
+        GLuint vertexBuffer;
+        std::array<float, noOfSquaresInCube> getVisibleFaces(glm::i8vec3 index) const noexcept;
         bool highlightedBlocks[chunkWidth][chunkHeight][chunkLength] {};
-        void highlightBlock(ChunkIndex index, float ambient) noexcept;
+        void highlightBlock(glm::i8vec3 index, float ambient) const noexcept;
+        std::array<std::optional<glm::i8vec3>, noOfSquaresInCube> getBlocksSurrounding(glm::i8vec3 index) const noexcept;
+        void updateChunkVisibility(glm::i8vec3 index) const noexcept;
     public:
-        void updateChunkVisibility() noexcept;
 
-        Chunk(BlockName firstLayer, BlockName bottomLayers) noexcept;
+        void updateChunkVisibilityToNeighbor(const Chunk &chunkNeighbor, Face face) const noexcept;
 
-        void modifyChunk(ChunkIndex chunkIndex, Block block) noexcept;
+        void updateChunkVisibility() const noexcept;
+
+        void initChunk(glm::vec3 position) noexcept;
+
+        void modifyChunk(glm::i8vec3 blockIndex, Block block) noexcept;
 
         inline void drawChunk() const noexcept
         {
-            vertexArray.bind();
+            glBindVertexArray(vertexArray);
             glDrawArrays(GL_TRIANGLES, 0, chunkVolume * attributesToFormCube);
         }
 
-        void updateBlocks() noexcept;
+        inline ~Chunk() noexcept
+        {
+            glDeleteBuffers(1, &vertexBuffer);
+            glDeleteVertexArrays(1, &vertexArray);
+        }
 
-        void updateBuffer() const noexcept;
+        void updateChunk(bool &updateNeeded) noexcept;
 
-        bool blockIsVisibleToPlayer(ChunkIndex index) const noexcept;
+        void updateBuffer(const ChunkVertex &chunkVertices) const noexcept;
+
+        void updateBuffer(size_t bufferIndex, Attribute attributeIndex, 
+                          std::span<const float> vertices, Face face=BackFace) const noexcept;
+
+        bool blockIsVisibleToPlayer(glm::i8vec3 index) const noexcept;
         
         /**
          * This function requires a 3D vector with 3 indices
          * to access the block. Returns true if any of the indices
          * are out of the chunk's array boundaries.
          */
-        static inline bool isOutOfChunk(ChunkIndex index) noexcept {
+        static inline bool isOutOfChunk(glm::i8vec3 index) noexcept {
             return ((index.x < 0 || index.y < 0 || index.z < 0) ||
                    (index.x >= chunkWidth || index.y >= chunkHeight || index.z >= chunkLength));
         }
