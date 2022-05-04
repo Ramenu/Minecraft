cfa871de9f57c7b0dbe625a26c9f5ae77630bf5d
Ramenu <abdurpro@gmail.com>
2022-05-04 14:50:15 -0400

    GDCT for-loop uses pre increment now

--- src/glmath.cpp
+++ src/glmath.cpp
@@ -20,7 +20,7 @@ namespace GLMath
         float dotOfClosest {glm::dot(directions[0], normVec)};
 
         // Iterate over all the directions and check which direction is the closest
-        for (uint8_t i {1}; i < 6; i++)
+        for (uint8_t i {1}; i < 6; ++i)
         {
             const float dot {glm::dot(directions[i], normVec)};
             if (dot > dotOfClosest)
