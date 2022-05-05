018beceee2031bfcb3572b0abafc938c74e14a5f
Ramenu <abdurpro@gmail.com>
2022-05-05 10:26:06 -0400

    constructor takes string as param now

--- src/sounddata.cpp
+++ src/sounddata.cpp
@@ -5,9 +5,9 @@
  * Initializes the sound from the filepath given by loading 
  * the sound into the buffer and setting the buffer into the audio. 
  */
-SoundData::SoundData(const char *soundFilePath) noexcept
+SoundData::SoundData(const std::string &soundFilePath) noexcept
 {
     if (!buffer.loadFromFile(soundFilePath))
-        GLError::error_message("Failed to initialize sound data from \"" + std::string{soundFilePath} + '\"');
+        GLError::error_message("Failed to initialize sound data from \"" + soundFilePath + '\"');
     audio.setBuffer(buffer);
 }
