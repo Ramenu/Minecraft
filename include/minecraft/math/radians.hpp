304626c87a872363e389115ce3fb5e2c45f28bba
Ramenu <abdurpro@gmail.com>
2022-05-06 19:44:53 -0400

    Updated radians class (and now in use, finally)

--- include/minecraft/math/radians.hpp
+++ include/minecraft/math/radians.hpp
@@ -2,14 +2,17 @@
 #define RADIANS_HPP
 
 #include <compare>
+#include "misc/boilerplate/glm_noerr.hpp"
 
 class Radians
 {
-    public:
-        explicit Radians(float angle) noexcept;
-        auto operator<=>(const Radians &r) const = default;
     private:
         float radians;
+    public:
+        inline explicit constexpr Radians(float angleInDegrees) noexcept : radians {glm::radians(angleInDegrees)} {}
+        inline constexpr float value() const noexcept {return radians;}
+        inline constexpr void set(float angleInDegrees) noexcept {radians = glm::radians(angleInDegrees);}
+        auto operator<=>(const Radians &r) const = default;
 };
 
 #endif // RADIANS_HPP
