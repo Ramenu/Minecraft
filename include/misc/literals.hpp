a6a732dcb40bee86e22b44ccb77e5f6db35dcc34
Ramenu <abdurpro@gmail.com>
2022-05-16 15:58:19 -0400

    use unsigned long long instead of size_t

--- include/misc/literals.hpp
+++ include/misc/literals.hpp
@@ -3,13 +3,13 @@
 
 #include <cstdint>
 
-inline constexpr int8_t operator"" _i8(size_t n) {return static_cast<int8_t>(n);}
-inline constexpr uint8_t operator"" _u8(size_t n) {return static_cast<uint8_t>(n);}
-inline constexpr int16_t operator"" _i16(size_t n) {return static_cast<int16_t>(n);}
-inline constexpr uint16_t operator"" _u16(size_t n) {return static_cast<uint16_t>(n);}
-inline constexpr int32_t operator"" _i32(size_t n) {return static_cast<int32_t>(n);}
-inline constexpr uint32_t operator"" _u32(size_t n) {return static_cast<uint32_t>(n);}
-inline constexpr int64_t operator "" _i64(size_t n) {return static_cast<int64_t>(n);}
-inline constexpr uint64_t operator"" _u64(uint64_t n) {return n;}
+inline constexpr std::int8_t operator"" _i8(unsigned long long n) {return static_cast<std::int8_t>(n);}
+inline constexpr std::uint8_t operator"" _u8(unsigned long long n) {return static_cast<std::uint8_t>(n);}
+inline constexpr std::int16_t operator"" _i16(unsigned long long n) {return static_cast<std::int16_t>(n);}
+inline constexpr std::uint16_t operator"" _u16(unsigned long long n) {return static_cast<std::uint16_t>(n);}
+inline constexpr std::int32_t operator"" _i32(unsigned long long n) {return static_cast<std::int32_t>(n);}
+inline constexpr std::uint32_t operator"" _u32(unsigned long long n) {return static_cast<std::uint32_t>(n);}
+inline constexpr std::int64_t operator "" _i64(unsigned long long n) {return static_cast<std::int64_t>(n);}
+inline constexpr std::uint64_t operator"" _u64(unsigned long long n) {return n;}
 
 #endif // LITERALS_HPP
