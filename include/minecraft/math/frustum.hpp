4b53bce741c206aa99d7ff1fa808b5d9ce52e237
Ramenu <abamansoorr@gmail.com>
2022-06-14 19:55:01 -0400

    revamp frustum class

--- include/minecraft/math/frustum.hpp
+++ include/minecraft/math/frustum.hpp
@@ -15,26 +15,22 @@ struct FrustumView
 class Frustum
 {
     private:
-        Plane nearFace;
-        Plane farFace;
-        Plane rightFace;
-        Plane leftFace;
-        Plane topFace;
-        Plane bottomFace;
+        Plane negZ;
+        Plane posZ;
+        Plane posX;
+        Plane negX;
+        Plane posY;
+        Plane negY;
     public:
+        // Near -> negZ
+        // Far -> posZ
         Frustum(const Plane &near, const Plane &far, const Plane &right,
                 const Plane &left, const Plane &top, const Plane &bottom) noexcept :
-                nearFace {near}, farFace {far}, rightFace {right},
-                leftFace {left}, topFace {top}, bottomFace {bottom} {}
+                negZ {near}, posZ {far}, posX {right},
+                negX {left}, posY {top}, negY {bottom} {}
 
         Frustum(const glm::vec3 &position, const Camera::CameraDirections &directions, const FrustumView &view) noexcept;
         [[nodiscard]] static Frustum getCameraFrustum() noexcept;
-        inline Plane getNearFace() const noexcept {return nearFace;}
-        inline Plane getFarFace() const noexcept {return farFace;}
-        inline Plane getRightFace() const noexcept {return rightFace;}
-        inline Plane getLeftFace() const noexcept {return leftFace;}
-        inline Plane getTopFace() const noexcept {return topFace;}
-        inline Plane getBottomFace() const noexcept {return bottomFace;}
 };
 
 #endif // MC_FRUSTUM_HPP
