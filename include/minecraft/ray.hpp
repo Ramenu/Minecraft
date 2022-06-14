54febefe3ac6d02e8556797e7f2b8c6f0b0a0201
Ramenu <abamansoorr@gmail.com>
2022-06-14 19:52:37 -0400

    revert ray class back to original

--- include/minecraft/ray.hpp
+++ include/minecraft/ray.hpp
@@ -3,25 +3,29 @@
 
 #include "glm/vec3.hpp"
 
-inline unsigned int lineProgram;
 
 class Ray
 {
-    public:
-        Ray() : origin {0.0f, 0.0f, 0.0f} {}
-        void initRay(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection, const glm::vec3 &rayLength) noexcept;
-        ~Ray() noexcept;
-        void updateRay(float xRotationRadians, float yRotationRadians, float zRotationRadians) noexcept;
-        inline glm::vec3 getRay() const noexcept {return ray;}
-        glm::vec3 origin;
-        glm::vec3 direction;
-        bool intersectsWith(const glm::vec3 &b) const noexcept;
-        uint32_t vertexBuffer;
-        uint32_t vao {};
-        glm::vec3 ray {};
-        glm::vec3 end {};
     private:
-        glm::vec3 length;
+        glm::vec3 ray {};
+    public:
+        Ray() = default;
+        inline constexpr Ray(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection) noexcept : ray {rayOrigin + rayDirection} {}
+        inline void updateRay(const glm::vec3 &origin, const glm::vec3 &direction) noexcept
+        {
+            ray = origin + direction;
+            #if 0
+                setTitle({static_cast<int8_t>(ray.x), static_cast<int8_t>(ray.y), static_cast<int8_t>(ray.z)});
+            #endif
+        }
+        inline glm::vec3 getRay() const noexcept {return ray;}
+        inline bool intersectsWith(const glm::vec3 &b) const noexcept
+        {
+            static constexpr float blockWidth {0.5f};
+            return (
+                (ray.x >= b.x - blockWidth && ray.x < b.x) &&
+                (ray.y >= b.y - blockWidth && ray.y < b.y + blockWidth));
+        }
 };
 
 #endif // MC_RAY_HPP
