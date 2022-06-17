58101aaab75f2f32a645ea8b4992eb5e0df27b7b
Ramenu <abamansoorr@gmail.com>
2022-06-17 13:49:04 -0400

    edit script

--- python/terraingen.py
+++ python/terraingen.py
@@ -30,8 +30,16 @@ def clamp(minN, maxN, n):
         return maxN
     return n
 
+def smoothstep(minN, maxN, n):
+    n = clamp(minN, maxN, n)
+    # Scale/bias into [0..1] range
+    n = (n - minN) / (maxN - minN)
+    return n * n * (3 - 2 * n)
 
-def perlin(p1 : float, p2 : float, gradients : list) -> float:
+
+def perlin(p1 : float, p2 : float) -> float:
+
+    gradients = generate2DGradients()
 
     point = [p1*0.01, p2*0.01]
     d1 = np.dot(point, gradients[0])
@@ -39,21 +47,20 @@ def perlin(p1 : float, p2 : float, gradients : list) -> float:
     d3 = np.dot(point, gradients[2])
     d4 = np.dot(point, gradients[3])
 
-    t1 = fade(p1)
-    t2 = fade(p2)
+    step1 = smoothstep(0.0, 1.0, p1)
+    step2 = smoothstep(0.0, 1.0, p2)
 
-    li1 = lerp(d1, d2, t1)
-    li2 = lerp(d3, d4, t1)
-    return lerp(li1, li2, t2)
+    li1 = lerp(d1, d2, step1)
+    li2 = lerp(d3, d4, step1)
+    return lerp(li1, li2, step2)
 
-def getColor(p1, p2, gradients) -> int:
-    p = perlin(p1, p2, gradients)
+def getColor(p1, p2) -> int:
+    p = perlin(p1, p2)
 
     # Transform range to [0.0, 1.0] assuming range of perlin is [-1.0, 1.0]
     p += 1.0
     p /= 2.0
-    p = clamp(0.0, 1.0, p)
-    return round(p*15.0)
+    return p
 
 def generateChunk(width : int, height : int):
     DO_PERLIN = 1
@@ -62,14 +69,11 @@ def generateChunk(width : int, height : int):
     else:
         print("Doing random")
     chunk = [[0] * width for i in range(height)]
-    longest = width
-    if height > longest:
-        longest = height
-    gradients = generate2DGradients()
+    longest = max(width, height)
     for x in range(width):
         for y in range(height):
             if DO_PERLIN:
-                chunk[x][y] = getColor(x, y, gradients, longest)
+                chunk[x][y] = getColor(x, y)
             else:
                 chunk[x][y] = random.randint(0, 1)
 
@@ -83,9 +87,9 @@ def generateChunk(width : int, height : int):
                 print("", end="")
         print("")
 
-p1 = 2.0
-p2 = 2.0
-gradients = generate2DGradients()
-print(getColor(p1, p2, gradients))
+#generateChunk(8, 8)
+p1 = 15.0
+p2 = 15.0
+print(round(getColor(p1, p2)*15))
 
 
