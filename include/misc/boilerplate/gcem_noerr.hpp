c9f6a7b9c0bbfc5641e6f09484eeb2e6901e1dbd
Ramenu <abamansoorr@gmail.com>
2022-06-20 11:10:42 -0400

    Add gcem_noerr.hpp

---
+++ include/misc/boilerplate/gcem_noerr.hpp
@@ -0,0 +1,9 @@
+#ifndef MC_GCEM_NOERR_HPP
+#define MC_GCEM_NOERR_HPP
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+#include "gcem/gcem.hpp"
+#pragma GCC diagnostic pop
+
+#endif // GCEM_NOERR_HPP
