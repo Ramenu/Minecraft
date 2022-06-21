2c4d0ca20ea1e057d85d4cdb720495ed23a9965e
Ramenu <abamansoorr@gmail.com>
2022-06-21 11:44:09 -0400

    revamp plane class

--- include/minecraft/math/plane.hpp
+++ include/minecraft/math/plane.hpp
@@ -2,16 +2,22 @@
 #define MC_PLANE_HPP
 
 #include "misc/boilerplate/glm_noerr.hpp"
+#include <cstdio>
 
 class Plane
 {
     public:
         Plane() = default;
         inline Plane(const glm::vec3 &vec, const glm::vec3 &norm) noexcept :
-            normal {glm::normalize(norm)},
+            normal {norm},
             distance {glm::dot(normal, vec)} {}
         glm::vec3 normal;
         float distance;
+        inline float getSignedDistanceToPlane(const glm::vec3 &point) const noexcept {
+            const float dist {glm::dot(normal, point) - distance};
+            //printf("%f\n", static_cast<double>(dist));
+            return dist;
+        }
 };
 
 #endif // MC_PLANE_HPP
