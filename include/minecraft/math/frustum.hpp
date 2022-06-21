481c780dfba9a38175e8ada5b89af727c37593d0
Ramenu <abamansoorr@gmail.com>
2022-06-21 11:43:51 -0400

    revamp frustum class (still unused)

--- include/minecraft/math/frustum.hpp
+++ include/minecraft/math/frustum.hpp
@@ -3,6 +3,7 @@
 
 #include "minecraft/math/plane.hpp"
 #include "minecraft/camera/camera.hpp"
+#include "minecraft/math/shape/sphere.hpp"
 
 struct FrustumView
 {
@@ -15,22 +16,31 @@ struct FrustumView
 class Frustum
 {
     private:
-        Plane negZ;
-        Plane posZ;
-        Plane posX;
-        Plane negX;
-        Plane posY;
-        Plane negY;
+        Plane nearFace;
+        Plane farFace;
+        Plane rightFace;
+        Plane leftFace;
+        Plane topFace;
+        Plane bottomFace;
     public:
-        // Near -> negZ
-        // Far -> posZ
+        Frustum() = default;
         Frustum(const Plane &near, const Plane &far, const Plane &right,
                 const Plane &left, const Plane &top, const Plane &bottom) noexcept :
-                negZ {near}, posZ {far}, posX {right},
-                negX {left}, posY {top}, negY {bottom} {}
+                nearFace {near}, farFace {far}, rightFace {right},
+                leftFace {left}, topFace {top}, bottomFace {bottom} {}
 
         Frustum(const glm::vec3 &position, const Camera::CameraDirections &directions, const FrustumView &view) noexcept;
         [[nodiscard]] static Frustum getCameraFrustum() noexcept;
+        inline bool isInFrustum(const Sphere &boundingSphere) const noexcept {
+            return (
+                boundingSphere.isOnForwardPlane(nearFace) &&
+                boundingSphere.isOnForwardPlane(farFace) &&
+                boundingSphere.isOnForwardPlane(rightFace) &&
+                boundingSphere.isOnForwardPlane(leftFace) &&
+                boundingSphere.isOnForwardPlane(topFace) &&
+                boundingSphere.isOnForwardPlane(bottomFace)
+            );
+        }
 };
 
 #endif // MC_FRUSTUM_HPP
--- src/frustum.cpp
+++ src/frustum.cpp
@@ -2,6 +2,7 @@
 #include "minecraft/math/glmath.hpp"
 #include "minecraft/window/window.hpp"
 #include "misc/boilerplate/gcem_noerr.hpp"
+#include "GLFW/glfw3.h"
 
 Frustum::Frustum([[maybe_unused]] const glm::vec3 &position, [[maybe_unused]] const Camera::CameraDirections &dir, [[maybe_unused]] const FrustumView &view) noexcept
 {
@@ -26,10 +27,71 @@ Frustum::Frustum([[maybe_unused]] const glm::vec3 &position, [[maybe_unused]] co
 
 Frustum Frustum::getCameraFrustum() noexcept
 {
-    /*static constexpr float HEIGHT_NEAR {2.0f * gcem::tan(Camera::FOV.value() / 2.0f) * Camera::NEAR};
-    static constexpr float WIDTH_NEAR {HEIGHT_NEAR * Window::ASPECT_RATIO};
+    Frustum frustum;
+    #if 1
+    static constexpr float HEIGHT_FAR_HALF {gcem::tan(Camera::FOV.value() / 2.0f) * Camera::FAR};
+    static constexpr float WIDTH_FAR_HALF {HEIGHT_FAR_HALF * Window::ASPECT_RATIO};
+    const glm::vec3 frontMultFar {Camera::direction.front * Camera::FAR};
+    const glm::vec3 rightNormal {glm::normalize(frontMultFar + Camera::direction.right * WIDTH_FAR_HALF)};
+    const glm::vec3 leftNormal {glm::normalize(frontMultFar - Camera::direction.right * WIDTH_FAR_HALF)};
+    const glm::vec3 upNormal {glm::normalize(frontMultFar - Camera::CAMERA_UP * HEIGHT_FAR_HALF)};
+    const glm::vec3 downNormal {glm::normalize(frontMultFar + Camera::CAMERA_UP * HEIGHT_FAR_HALF)};
 
-    static constexpr float HEIGHT_FAR {2.0f * gcem::tan(Camera::FOV.value() / 2.0f) * Camera::FAR};
-    static constexpr float WIDTH_FAR {HEIGHT_FAR * Window::ASPECT_RATIO};*/
-    return Frustum{Plane{}, Plane{}, Plane{}, Plane{}, Plane{}, Plane{}};
+    frustum.nearFace = {Camera::cameraPos + Camera::FAR * Camera::direction.front, Camera::direction.front};
+    frustum.farFace = {Camera::cameraPos + frontMultFar, -Camera::direction.front};
+    frustum.rightFace = {Camera::cameraPos, glm::normalize(glm::cross(Camera::CAMERA_UP, rightNormal))};
+    frustum.leftFace = {Camera::cameraPos, glm::normalize(glm::cross(leftNormal, Camera::CAMERA_UP))};
+    frustum.topFace = {Camera::cameraPos, glm::normalize(glm::cross(Camera::direction.right, upNormal))};
+    frustum.bottomFace = {Camera::cameraPos, glm::normalize(glm::cross(downNormal, Camera::direction.right))};
+
+    /*if (glfwGetKey(Window::getWindow(), GLFW_KEY_Z) == GLFW_PRESS)
+    {
+        printf("----------\n"
+               "Near face: %f\n"
+               "Far face: %f\n"
+               "Right face: %f\n"
+               "Left face: %f\n", static_cast<double>(frustum.farFace.distance),
+                                   static_cast<double>(frustum.nearFace.distance),
+                                   static_cast<double>(frustum.rightFace.distance),
+                                   static_cast<double>(frustum.leftFace.distance));
+    }*/
+    #endif
+
+    return frustum;
+
 }
+
+/*
+bool Frustum::isInFrustum(const glm::vec3 &position, const glm::vec3 &offset) const noexcept
+{
+    const glm::vec3 posOffset {position + offset};
+    #if 1
+    const float distToNear {nearFace.getSignedDistanceToPlane(position)};
+    const float distToFar {farFace.getSignedDistanceToPlane(posOffset)};
+    
+    const float distToBottom {bottomFace.getSignedDistanceToPlane(position)};
+    const float distToTop {topFace.getSignedDistanceToPlane(position)};
+    if (glfwGetKey(Window::getWindow(), GLFW_KEY_Z) == GLFW_PRESS)
+    {
+        printf("----------\n"
+               "Distance to top: %f\n"
+               "Distance to bottom: %f\n", static_cast<double>(distToTop),
+                                        static_cast<double>(distToBottom));
+        printf("Bottom face distance: %f\n", static_cast<double>(distToTop));
+    }
+
+    return (nearFace.distance >= distToNear && farFace.distance <= distToFar) &&
+           (bottomFace.distance >= distToBottom && topFace.distance <= distToTop);
+    #else
+    const float distToBottom {glm::dot(position, bottomFace.normal)};
+    const float distToUp {glm::dot(glm::normalize(position + offset), topFace.normal)};
+
+    if (glfwGetKey(Window::getWindow(), GLFW_KEY_Z) == GLFW_PRESS)
+    {
+        printf("----------\n"
+               "Distance to top: %f\n", static_cast<double>(topFace.distance - distToUp));
+    }
+
+    return (bottomFace.distance >= distToBottom && topFace.distance <= distToUp);
+    #endif
+}*/
