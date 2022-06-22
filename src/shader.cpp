b93760d576894ae2a833d696ab852937bedf58af
Ramenu <abamansoorr@gmail.com>
2022-06-22 11:52:20 -0400

    init shaderProgram in initializer list

--- src/shader.cpp
+++ src/shader.cpp
@@ -10,10 +10,13 @@
  * If there are any errors during compilation or linking then 
  * it will print them to stderr. 
  */
-Shader::Shader(const std::string &vertexShaderSource, const std::string &fragmentShaderSource) noexcept
+Shader::Shader(const std::string &vertexShaderSource, const std::string &fragmentShaderSource) noexcept : 
+    shaderProgram{glCreateProgram()}
 {
-    std::stringstream vertexShaderStream, fragmentShaderStream;
-    std::ifstream vertexShaderFile, fragmentShaderFile;
+    std::stringstream vertexShaderStream;
+    std::stringstream fragmentShaderStream;
+    std::ifstream vertexShaderFile;
+    std::ifstream fragmentShaderFile;
 
     vertexShaderFile.open(vertexShaderSource);
     fragmentShaderFile.open(fragmentShaderSource);
@@ -29,7 +32,7 @@ Shader::Shader(const std::string &vertexShaderSource, const std::string &fragmen
     const std::string vertexShaderCode {vertexShaderStream.str()};
     const char *cstrVertexShaderCode {vertexShaderCode.c_str()};
     const GLuint vertexShader {glCreateShader(GL_VERTEX_SHADER)};
-    glShaderSource(vertexShader, 1, &cstrVertexShaderCode, NULL);
+    glShaderSource(vertexShader, 1, &cstrVertexShaderCode, nullptr);
     glCompileShader(vertexShader);
     checkShaderCompilationErrors(vertexShader, vertexShaderSource);
 
@@ -37,11 +40,10 @@ Shader::Shader(const std::string &vertexShaderSource, const std::string &fragmen
     const std::string fragmentShaderCode {fragmentShaderStream.str()};
     const char *cstrFragmentShaderCode {fragmentShaderCode.c_str()};
     const GLuint fragmentShader {glCreateShader(GL_FRAGMENT_SHADER)};
-    glShaderSource(fragmentShader, 1, &cstrFragmentShaderCode, NULL);
+    glShaderSource(fragmentShader, 1, &cstrFragmentShaderCode, nullptr);
     glCompileShader(fragmentShader);
     checkShaderCompilationErrors(fragmentShader, fragmentShaderSource);
     
-    shaderProgram = glCreateProgram();
 
     // Attach the shaders together and link the program (and check for errors)
     glAttachShader(shaderProgram, vertexShader);
@@ -59,15 +61,16 @@ Shader::Shader(const std::string &vertexShaderSource, const std::string &fragmen
  * the program will print an error message to stderr before
  * terminating the program.
  */
-void Shader::checkShaderCompilationErrors(GLuint shader, const std::string &shaderPath) const noexcept
+void Shader::checkShaderCompilationErrors(GLuint shader, const std::string &shaderPath) noexcept
 {
+    static constexpr int UNSUCCESSFUL_COMPILATION {0};
     int success {};
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success)
+    if (success == UNSUCCESSFUL_COMPILATION)
     {
         static constexpr std::size_t INFO_LOG_SIZE {512};
         char infoLog[INFO_LOG_SIZE] {};
-        glGetShaderInfoLog(shader, INFO_LOG_SIZE, NULL, static_cast<char*>(infoLog));
+        glGetShaderInfoLog(shader, INFO_LOG_SIZE, nullptr, static_cast<char*>(infoLog));
         GLError::error_message("Failed to compile shader \"" + shaderPath + "\" " + std::string{infoLog});
     }
 }
@@ -75,13 +78,14 @@ void Shader::checkShaderCompilationErrors(GLuint shader, const std::string &shad
 /* Checks if the program linking was unsuccessful. */
 void Shader::checkLinkageErrors() const noexcept
 {
+    static constexpr int UNSUCCESSFUL_LINK {0};
     int success {};
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
+    if (success == UNSUCCESSFUL_LINK)
     {
         static constexpr std::size_t INFO_LOG_SIZE {512};
         char infoLog[INFO_LOG_SIZE] {};
-        glGetProgramInfoLog(shaderProgram, INFO_LOG_SIZE, NULL, static_cast<char*>(infoLog));
+        glGetProgramInfoLog(shaderProgram, INFO_LOG_SIZE, nullptr, static_cast<char*>(infoLog));
         GLError::error_message("Failed to link shader program:\n" + std::string{infoLog});
     }
 }
