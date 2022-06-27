e7f8f9dab5a2ceb3a54e8cdf61322f84b1707b52
ramenu <abamansoorr@gmail.com>
2022-06-27 16:19:10 -0400

    remove intersectsWith method

--- include/minecraft/ray.hpp
+++ include/minecraft/ray.hpp
@@ -2,6 +2,7 @@
 #define MC_RAY_HPP
 
 #include "glm/vec3.hpp"
+#include "minecraft/debug/debug.hpp"
 
 
 class Ray
@@ -13,19 +14,14 @@ class Ray
         inline constexpr Ray(const glm::vec3 &rayOrigin, const glm::vec3 &rayDirection) noexcept : ray {rayOrigin + rayDirection} {}
         inline void updateRay(const glm::vec3 &origin, const glm::vec3 &direction) noexcept
         {
-            ray = origin + direction;
+            static constexpr float distance {2.0f};
+            ray = origin + direction * distance;
             #if 0
-                setTitle({static_cast<int8_t>(ray.x), static_cast<int8_t>(ray.y), static_cast<int8_t>(ray.z)});
+                //setTitle({static_cast<int8_t>(ray.x), static_cast<int8_t>(ray.y), static_cast<int8_t>(ray.z)});
+                setTitle(ray);
             #endif
         }
         inline glm::vec3 getRay() const noexcept {return ray;}
-        inline bool intersectsWith(const glm::vec3 &b) const noexcept
-        {
-            static constexpr float blockWidth {0.5f};
-            return (
-                (ray.x >= b.x - blockWidth && ray.x < b.x) &&
-                (ray.y >= b.y - blockWidth && ray.y < b.y + blockWidth));
-        }
 };
 
 #endif // MC_RAY_HPP
