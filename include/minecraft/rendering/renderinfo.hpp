0f34f65b1cdc71cf0281f19123006d6d3c35f8c1
ramenu <abamansoorr@gmail.com>
2022-06-28 21:47:15 -0400

    changed x, z render distance to 5

--- include/minecraft/rendering/renderinfo.hpp
+++ include/minecraft/rendering/renderinfo.hpp
@@ -1,7 +1,7 @@
 #ifndef MC_RENDERINFO_HPP
 #define MC_RENDERINFO_HPP
 
-static constexpr int RENDER_DISTANCE_X {4};
-static constexpr int RENDER_DISTANCE_Z {4};
+static constexpr int RENDER_DISTANCE_X {5};
+static constexpr int RENDER_DISTANCE_Z {5};
 
 #endif // MC_RENDERINFO_HPP
