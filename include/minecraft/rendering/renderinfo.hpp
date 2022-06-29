7a70029bcb096fabc88418dfbc70c2532eaa7994
ramenu <abamansoorr@gmail.com>
2022-06-29 12:28:08 -0400

    increase render distance to 6

--- include/minecraft/rendering/renderinfo.hpp
+++ include/minecraft/rendering/renderinfo.hpp
@@ -1,7 +1,7 @@
 #ifndef MC_RENDERINFO_HPP
 #define MC_RENDERINFO_HPP
 
-static constexpr int RENDER_DISTANCE_X {5};
-static constexpr int RENDER_DISTANCE_Z {5};
+static constexpr int RENDER_DISTANCE_X {6};
+static constexpr int RENDER_DISTANCE_Z {6};
 
 #endif // MC_RENDERINFO_HPP
