81dd957ac43ab5b940f46d432996ad0dc1ee67fd
ramenu <abamansoorr@gmail.com>
2022-06-28 21:11:45 -0400

    Check if running in debug mode before running game

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,18 @@
 #include "minecraft/glerror/glerror.hpp"
 #include "misc/terminal.h"
 #include "GLFW/glfw3.h"
+#include <cstring>
+#include "minecraft/window/window.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
     static_assert(sizeof(std::size_t) >= 4, "Go buy a real computer");
+
+    if (argc > 1)
+        for (int i {}; i < argc; ++i)
+            if (strcmp(argv[i], "-debug") == 0)
+                Window::runningInDebugMode = true;
+
     #if defined(__MINGW32__) || defined(__WIN32__) || defined(WIN32) || defined(_WIN32)
         if (enable_virtual_terminal_processing()) 
         {
