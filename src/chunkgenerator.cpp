a950b0bcdc770ccc48b482db0ff1e16b9b9a612c
ramenu <abamansoorr@gmail.com>
2022-06-29 22:36:57 -0400

    generate forest biomes instead of plains (WIP)

--- src/chunkgenerator.cpp
+++ src/chunkgenerator.cpp
@@ -47,7 +47,7 @@ namespace ChunkGenerator
             {
                 const glm::i32vec3 chunkOffset {x, 0, z};
                 if (mappedBiomes.find(chunkOffset) == mappedBiomes.end())
-                    mappedBiomes.insert({chunkOffset, initChunkData(Plains, chunkOffset)});
+                    mappedBiomes.insert({chunkOffset, initChunkData(Forest, chunkOffset)});
 
                 const auto currentCameraOffset {Camera::getCameraPosChunkOffset()};
 
