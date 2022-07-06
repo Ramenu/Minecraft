5d52e9ddfb079eee83462e1ec91d859c05279711
ramenu <abamansoorr@gmail.com>
2022-07-06 18:25:38 -0400

    include n define only on debug builds

--- include/minecraft/debug/debug.hpp
+++ include/minecraft/debug/debug.hpp
@@ -1,15 +1,17 @@
 #ifndef MC_DEBUG_HPP
 #define MC_DEBUG_HPP
 
-#include "glm/vec3.hpp"
-#include "minecraft/window/window.hpp"
-#include <string>
-#include <cmath>
+#ifndef NDEBUG
+    #include "glm/vec3.hpp"
+    #include "minecraft/window/window.hpp"
+    #include <string>
+    #include <cmath>
 
-inline void setTitle(const glm::vec3 &vec) noexcept
-{
-    std::string s {std::to_string(vec.x) + ", " + std::to_string(vec.y) + ", " + std::to_string(vec.z)};
-    glfwSetWindowTitle(Window::getWindow(), s.c_str());
-}
+    inline void setTitle(const glm::vec3 &vec) noexcept
+    {
+        std::string s {std::to_string(vec.x) + ", " + std::to_string(vec.y) + ", " + std::to_string(vec.z)};
+        glfwSetWindowTitle(Window::getWindow(), s.c_str());
+    }
+#endif
 
 #endif // MC_DEBUG_HPP
